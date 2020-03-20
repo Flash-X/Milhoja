500a27a1c0d83dbc928ac5aa98e7032560cbc003
Jared O'Neal <joneal@anl.gov>
2020-03-20 15:19:33 -0500

    WIP: Removed some C++ contagion from the computeError Fortran code.  Scaled up problem size a bit.

--- test/Analysis_computeErrors.F90
+++ test/Analysis_computeErrors.F90
@@ -24,14 +24,18 @@ subroutine Analysis_computeErrors(tId, tilePtr) bind(c)
     real,       allocatable                     :: yCoords(:)
     real,       allocatable                     :: zCoords(:)
 
-    real    :: fExpected      = 0.0
-    real    :: absErr         = 0.0
-    real    :: sumDens        = 0.0
-    real    :: sumEner        = 0.0
-    integer :: nCells         = 0
+    real    :: fExpected
+    real    :: absErr
+    real    :: LinfDens
+    real    :: LinfEner
+    real    :: sumDens
+    real    :: sumEner
+    integer :: nCells
 
     real    :: x, y, z
     integer :: i, j, k
+
+    nullify(f)
 
     tileDesc = tilePtr
 
@@ -52,7 +56,11 @@ subroutine Analysis_computeErrors(tId, tilePtr) bind(c)
     ! These variables are shared resources being accessed in parallel
     ! If we assume that the grid indices are unique and that each tile is
     ! only once, then there is no possibility for resource contention.
-    an_LinfErrors(:, idx) = 0.0
+    nCells = 0
+    sumDens = 0.0
+    sumEner = 0.0
+    LinfDens = 0.0
+    LinfEner = 0.0
     do         k = lo(KAXIS), hi(KAXIS)
         z = zCoords(k)
         do     j = lo(JAXIS), hi(JAXIS)
@@ -65,7 +73,7 @@ subroutine Analysis_computeErrors(tId, tilePtr) bind(c)
                             + 5.0;
                 absErr = ABS(f(i, j, k, DENS_VAR) - fExpected)
                 sumDens = sumDens + absErr
-                an_LinfErrors(DENS_VAR, idx) = MAX(an_LinfErrors(DENS_VAR, idx), absErr)
+                LinfDens = MAX(LinfDens, absErr)
 
                 fExpected =   4.0*x*x*x*x - 3.0*x*x*x + 2.0*x*x -     x &
                             -     y*y*y*y + 2.0*y*y*y - 3.0*y*y + 4.0*y &
@@ -73,16 +81,19 @@ subroutine Analysis_computeErrors(tId, tilePtr) bind(c)
                 fExpected = fExpected * an_energyFactor
                 absErr = ABS(f(i, j, k, ENER_VAR) - fExpected)
                 sumEner = sumEner + absErr
-                an_LinfErrors(ENER_VAR, idx) = MAX(an_LinfErrors(ENER_VAR, idx), absErr)
+                LinfEner = MAX(LinfEner, absErr)
 
                 nCells = nCells + 1
             end do
         end do
     end do
 
+    an_LinfErrors(DENS_VAR, idx) = LinfDens
+    an_LinfErrors(ENER_VAR, idx) = LinfEner
     an_meanAbsErrors(DENS_VAR, idx) = sumDens / REAL(nCells)
     an_meanAbsErrors(ENER_VAR, idx) = sumEner / REAL(nCells)
 
     deallocate(xCoords, yCoords, zCoords)
+    nullify(f)
 end subroutine Analysis_computeErrors
 
--- test/Flash.h
+++ test/Flash.h
@@ -13,8 +13,8 @@
 #define Y_MAX      1.0
 #define Z_MIN      0.0
 #define Z_MAX      0.0
-#define N_BLOCKS_X   2
-#define N_BLOCKS_Y   1 
+#define N_BLOCKS_X  64
+#define N_BLOCKS_Y  32
 #define N_BLOCKS_Z   1
 
 #endif
