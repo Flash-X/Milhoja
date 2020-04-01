d5ece86974887a2e47a91d9986d9ac192546f090
Jared O'Neal <joneal@anl.gov>
2020-04-01 18:38:27 -0500

    WIP: Updated RuntimeCpp test for hello.

--- sites/hello/testRuntimeCpp.sh
+++ sites/hello/testRuntimeCpp.sh
@@ -15,20 +15,20 @@ BINARY=test_runtime_cpp.x
 TESTDIR=../../test
 
 # Setup constants.h with current simulation's Grid parameters
-rm $TESTDIR/constants.h
+rm $TESTDIR/Runtime/constants.h
 sed "s/N_CELLS_IN_X/$N_CELLS_IN_X/g" \
         $TESTDIR/constants_base.h > \
-        $TESTDIR/constants.h
-sed -i '' "s/N_CELLS_IN_Y/$N_CELLS_IN_Y/g" $TESTDIR/constants.h
-sed -i '' "s/N_CELLS_IN_Z/$N_CELLS_IN_Z/g" $TESTDIR/constants.h
+        $TESTDIR/Runtime/constants.h
+sed -i '' "s/N_CELLS_IN_Y/$N_CELLS_IN_Y/g" $TESTDIR/Runtime/constants.h
+sed -i '' "s/N_CELLS_IN_Z/$N_CELLS_IN_Z/g" $TESTDIR/Runtime/constants.h
 
 # Setup Flash.h with current simulation's Grid parameters
-rm $TESTDIR/Flash.h
+rm $TESTDIR/Runtime/Flash.h
 sed "s/N_BLOCKS_ALONG_X/$N_BLOCKS_X/g" \
         $TESTDIR/Flash_base.h > \
-        $TESTDIR/Flash.h
-sed -i '' "s/N_BLOCKS_ALONG_Y/$N_BLOCKS_Y/g" $TESTDIR/Flash.h
-sed -i '' "s/N_BLOCKS_ALONG_Z/$N_BLOCKS_Z/g" $TESTDIR/Flash.h
+        $TESTDIR/Runtime/Flash.h
+sed -i '' "s/N_BLOCKS_ALONG_Y/$N_BLOCKS_Y/g" $TESTDIR/Runtime/Flash.h
+sed -i '' "s/N_BLOCKS_ALONG_Z/$N_BLOCKS_Z/g" $TESTDIR/Runtime/Flash.h
 
 # Build test binary
 if   [[ "$#" -eq 0 ]]; then
@@ -50,8 +50,8 @@ if [[ $? -ne 0 ]]; then
     echo "Unable to compile $BINARY"
     exit 3;
 fi
-rm $TESTDIR/Flash.h
-rm $TESTDIR/constants.h
+rm $TESTDIR/Runtime/Flash.h
+rm $TESTDIR/Runtime/constants.h
 
 time ./$BINARY
 if [[ $? -ne 0 ]]; then
