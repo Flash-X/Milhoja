1209282805aa81cd2b0162c890d2e6cf312eace9
Jared O'Neal <oneal@lcrc.anl.gov>
2020-04-01 12:53:14 -0500

    WIP: Still adding jobs, but for Bebop now.  I'll need a different site for LCRC Jenkins.

--- test/runRuntimeCppTest.cpp
+++ test/runRuntimeCppTest.cpp
@@ -14,6 +14,15 @@ int main(int argc, char* argv[]) {
     // Initialize Grid unit/AMReX
     Grid<NXB,NYB,NZB,NGUARD>*    grid = Grid<NXB,NYB,NZB,NGUARD>::instance();
 
+    int  rank = -1;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+    if (rank != 0) {
+        delete listeners.Release(listeners.default_result_printer());
+    }
+
     int  errorCode = RUN_ALL_TESTS();
 
     // Finalize Grid unit/AMReX
