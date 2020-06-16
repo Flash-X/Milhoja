b437a87e68e65d93433fe9470a128b2caec1529c
Thomas Klosterman <tklos96@gmail.com>
2020-06-16 08:34:54 -0400

    created make and buildfile for a Grid testsuite

---
+++ test/Grid/runGridTest.cpp
@@ -0,0 +1,17 @@
+#include <AMReX.H>
+#include "Grid.h"
+//#include "Flash.h"
+//#include "constants.h"
+#include <gtest/gtest.h>
+
+int main(int argc, char* argv[]) {
+        ::testing::InitGoogleTest(&argc, argv);
+
+        Grid::instance();
+
+        //int rank = -1;
+        //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+        return RUN_ALL_TESTS();
+}
+
