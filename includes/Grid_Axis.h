3939d598856ace1993d65e3f2c6aff3dd9f041c2
Thomas Klosterman <tklos96@gmail.com>
2020-07-16 15:41:09 -0400

    Axis and Edge namespaces for relevant constants

--- includes/Grid_Axis.h
+++ includes/Grid_Axis.h
@@ -4,9 +4,9 @@
 namespace orchestration {
 
 namespace Axis{
-    static constexpr unsigned int I = 0;
-    static constexpr unsigned int J = 1;
-    static constexpr unsigned int K = 2;
+    constexpr unsigned int I = 0;
+    constexpr unsigned int J = 1;
+    constexpr unsigned int K = 2;
 }
 
 
