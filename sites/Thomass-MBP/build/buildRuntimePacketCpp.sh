5b818a28909c1c46d717c3057c25b1129752fe6e
Thomas Klosterman <tklos96@gmail.com>
2020-08-04 13:41:19 -0500

    run RuntimePacketCpp test locally

---
+++ sites/Thomass-MBP/build/buildRuntimePacketCpp.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Define test problem - we need dx=dy
+N_CELLS_IN_X=8
+N_CELLS_IN_Y=16
+N_CELLS_IN_Z=1
+
+N_BLOCKS_X=256
+N_BLOCKS_Y=128
+N_BLOCKS_Z=1
+
+cd ..
+
+TESTDIR=../../test
+MAKEFILE=Makefile_runtime_packet_cpp
+BINARY=test_runtime_packet_cpp.x 
+
+
+# Setup constants.h with current simulation's Grid parameters
+rm $TESTDIR/RuntimePacket/constants.h
+sed "s/N_CELLS_IN_X/$N_CELLS_IN_X/g" \
+        $TESTDIR/constants_base.h > \
+        $TESTDIR/RuntimePacket/constants.h
+sed -i '' "s/N_CELLS_IN_Y/$N_CELLS_IN_Y/g" $TESTDIR/RuntimePacket/constants.h
+sed -i '' "s/N_CELLS_IN_Z/$N_CELLS_IN_Z/g" $TESTDIR/RuntimePacket/constants.h
+sed -i '' "s/N_DIMENSIONS/2/g"             $TESTDIR/RuntimePacket/constants.h
+
+# Setup Flash.h with current simulation's Grid parameters
+rm $TESTDIR/RuntimePacket/Flash.h
+sed "s/N_BLOCKS_ALONG_X/$N_BLOCKS_X/g" \
+        $TESTDIR/Flash_base.h > \
+        $TESTDIR/RuntimePacket/Flash.h
+sed -i '' "s/N_BLOCKS_ALONG_Y/$N_BLOCKS_Y/g" $TESTDIR/RuntimePacket/Flash.h
+sed -i '' "s/N_BLOCKS_ALONG_Z/$N_BLOCKS_Z/g" $TESTDIR/RuntimePacket/Flash.h
+sed -i '' "s/REFINEMENT_LEVELS/1/g"          $TESTDIR/RuntimePacket/Flash.h
+
+# Build test binary
+if   [[ "$#" -eq 0 ]]; then
+        make -f $MAKEFILE clean all
+elif [[ "$#" -eq 1 ]]; then
+    if [[ "$1" = "--debug" ]]; then
+        echo "No debug-compatible tests in suite"
+        exit 5;
+#        make -f $MAKEFILE clean all DEBUG=T
+    else
+        echo "Unknown command line argument", $1
+        exit 1;
+    fi
+elif [[ "$#" -gt 1 ]]; then
+    echo "At most one command line argument accepted"
+    exit 2;
+fi
+
+# Confirm build and clean-up
+if [[ $? -ne 0 ]]; then
+    echo "Unable to compile $BINARY"
+    exit 3;
+fi
+mv $BINARY ./binaries/$BINARY
+
+rm $TESTDIR/RuntimePacket/Flash.h
+rm $TESTDIR/RuntimePacket/constants.h
