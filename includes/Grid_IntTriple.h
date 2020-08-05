941351c51da1ea79b7f6eaa6521e47fa095ade17
Thomas Klosterman <tklos96@gmail.com>
2020-08-05 13:12:02 -0500

    inline docs for IntTriple

--- includes/Grid_IntTriple.h
+++ includes/Grid_IntTriple.h
@@ -13,29 +13,40 @@
 
 namespace orchestration {
 
+/**
+  * \brief Container for MDIM-tuples of integers.
+  *
+  * A very "weak" class - it has no math operators defined and cannot
+  * be converted into any other data type. Should be used only for
+  * triple nested loops and for defining bounds of FArray4Ds.
+  *
+  */
 class IntTriple
 {
   public:
 
-    // Generic constructor, returns a vector with undefined components.
-    // TODO: return a default value aka 0?
+    /** \brief Default constructor
+      *
+      * Returns a vector with undefined components.
+      * TODO: return a default value aka 0?
+      */
     explicit IntTriple () {}
 
-    // Constructor from MDIM ints.
-    constexpr explicit IntTriple (const int x, const int y, const int z) : vect_{x,y,z} {}
+    //! Constructor from MDIM ints.
+    constexpr explicit IntTriple (const int x, const int y, const int z)
+        : vect_{x,y,z} {}
 
 #ifdef GRID_AMREX
-    // Constructor from amrex::Dim3
+    //! Constructor from amrex::Dim3
     explicit IntTriple (const amrex::Dim3& ain) : vect_{ain.x,ain.y,ain.z} {}
 
-    // Operator to explicitly cast an IntTriple to an AMReX Dim3
+    //! Operator to explicitly cast an IntTriple to an AMReX Dim3
     explicit operator amrex::Dim3 () const {
         return amrex::IntVect(LIST_NDIM(vect_[0],vect_[1],vect_[2])).dim3() ;
     }
 #endif
 
-    /* A Note on move/copy sematics.
-       */
+    // Allow move semantics but no copies.
     IntTriple(IntTriple&&) = default;
     IntTriple& operator=(IntTriple&&) = default;
     IntTriple(IntTriple&) = delete;
@@ -43,17 +54,21 @@ class IntTriple
     IntTriple& operator=(IntTriple&) = delete;
     IntTriple& operator=(const IntTriple&) = delete;
 
-
-    // Get and set values of the internal array with [] operator.
+    //! Get and set values of the internal array.
     int& operator[] (const int i) {
 #ifndef GRID_ERRCHECK_OFF
-        if(i>=MDIM || i<0) throw std::logic_error("Index out-of-bounds in IntTriple.");
+        if(i>=MDIM || i<0) {
+            throw std::logic_error("Index out-of-bounds in IntTriple.");
+        }
 #endif
         return vect_[i];
     }
+    //! Get values of the internal array as consts.
     const int& operator[] (const int i) const {
 #ifndef GRID_ERRCHECK_OFF
-        if(i>=MDIM || i<0) throw std::logic_error("Index out-of-bounds in IntTriple.");
+        if(i>=MDIM || i<0) {
+            throw std::logic_error("Index out-of-bounds in IntTriple.");
+        }
 #endif
         return vect_[i];
     }
@@ -62,8 +77,8 @@ class IntTriple
     //friend std::ostream& operator<< (std::ostream& os, const IntTriple& vout);
 
   private:
-    int vect_[MDIM];
+    int vect_[MDIM];   //!< Contains data.
 };
 
-} //namespace orchestration
+}
 #endif
