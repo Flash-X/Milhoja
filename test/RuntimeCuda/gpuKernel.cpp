20f5e081764ab1166cb2b5a1c4705d802ac4f85e
Jared O'Neal <joneal@anl.gov>
2020-08-28 17:24:21 -0400

    (Issue #8) Updated the test so that it now uses a minimal data packet.  Still using synchronous transfers and not using FArray4D yet.

--- test/RuntimeCuda/gpuKernel.cpp
+++ test/RuntimeCuda/gpuKernel.cpp
@@ -1,14 +1,27 @@
 #include "gpuKernel.h"
 
+#include <cstdlib>
+
+#include "Grid_REAL.h"
+
 using namespace orchestration;
 
-void   gpuKernel::kernel(Real* data_d, const std::size_t nCells) {
-        #pragma acc data copyin(nCells) deviceptr(data_d)
-        {
-        #pragma acc parallel loop
-        for (std::size_t i=0; i<nCells; ++i) {
-            data_d[i] += 2.1;
-        }
-        }
+void   gpuKernel::kernel(void* packet_d) {
+    // Unmarshall data from given data packet
+    char*  p = static_cast<char*>(packet_d);
+
+    const std::size_t*  nCells_d = reinterpret_cast<std::size_t*>(p);
+    p += sizeof(std::size_t);
+    Real*               data_d   = reinterpret_cast<Real*>(p);
+
+    // Run our kernel in the device with the anticipation
+    // that the data is alredy present in the device memory
+    #pragma acc data deviceptr(nCells_d, data_d)
+    {
+    #pragma acc parallel loop default(none)
+    for (std::size_t i=0; i<*nCells_d; ++i) {
+        data_d[i] += 2.1; 
+    }
+    }
 }
 
--- test/RuntimeCuda/testCudaKernel.cu
+++ test/RuntimeCuda/testCudaKernel.cu
@@ -47,14 +47,26 @@ int   main(int argc, char* argv[]) {
     Grid&    grid = Grid::instance();
     grid.initDomain(setInitialConditions_block);
 
-    Real*         data_d = nullptr;
-    std::size_t   nCells =   (NXB + 2 * NGUARD * K1D)
-                           * (NYB + 2 * NGUARD * K2D)
-                           * (NZB + 2 * NGUARD * K3D)
-                           * NUNKVAR;
-    std::size_t   nBytes = nCells * sizeof(Real);
+    // Determine size of and allocate memory for data packet
+    constexpr std::size_t   N_CELLS =   (NXB + 2 * NGUARD * K1D)
+                                      * (NYB + 2 * NGUARD * K2D)
+                                      * (NZB + 2 * NGUARD * K3D)
+                                      * NUNKVAR;
+    constexpr std::size_t   N_BYTES =         1 * sizeof(std::size_t) 
+                                      + N_CELLS * sizeof(Real);
 
-    cudaError_t    cErr = cudaMalloc(&data_d, nBytes);
+    void*         packet_p = nullptr;
+    cudaError_t   cErr = cudaMallocHost(&packet_p, N_BYTES);
+    if (cErr != cudaSuccess) {
+        std::string  errMsg = "[CudaDataPacket::prepareForTransfer] ";
+        errMsg += "Unable to allocate pinned memory\n";
+        errMsg += "Cuda error - " + std::string(cudaGetErrorName(cErr)) + "\n";
+        errMsg += std::string(cudaGetErrorString(cErr)) + "\n";
+        throw std::runtime_error(errMsg);
+    }
+
+    void*         packet_d = nullptr;
+    cErr = cudaMalloc(&packet_d, N_BYTES);
     if (cErr != cudaSuccess) {
         std::string  errMsg = "[CudaDataPacket::prepareForTransfer] ";
         errMsg += "Unable to allocate device memory\n";
@@ -64,14 +76,33 @@ int   main(int argc, char* argv[]) {
     }
 
     // Run the kernel in the CPU at first
+    Real*   data_h = nullptr;
+    Real*   data_p = nullptr;
+    Real*   data_d = nullptr;
+    char*   ptr_p  = nullptr;
+    char*   ptr_d  = nullptr;
+    assert(sizeof(char) == 1);
     for (auto ti = grid.buildTileIter(LEVEL); ti->isValid(); ti->next()) {
         std::unique_ptr<Tile>   tileDesc = ti->buildCurrentTile();
 
-        const IntVect   loGC   = tileDesc->loGC();
-        const IntVect   hiGC   = tileDesc->hiGC();
-        Real*           data_h = tileDesc->dataPtr();
+        data_h = tileDesc->dataPtr();
 
-        cErr = cudaMemcpy(data_d, data_h, nBytes, cudaMemcpyHostToDevice);
+        // Marshall data for single tile data packet
+        ptr_p = static_cast<char*>(packet_p);
+        ptr_d = static_cast<char*>(packet_d);
+
+        std::size_t   nCells = N_CELLS;
+        std::memcpy((void*)ptr_p, (void*)&nCells, sizeof(std::size_t));
+        ptr_p += sizeof(std::size_t);
+        ptr_d += sizeof(std::size_t);
+
+        data_p = reinterpret_cast<Real*>(ptr_p);
+        data_d = reinterpret_cast<Real*>(ptr_d);
+        std::memcpy((void*)ptr_p, (void*)data_h, N_CELLS*sizeof(Real));
+        ptr_p += N_CELLS*sizeof(Real);
+        ptr_d += N_CELLS*sizeof(Real);
+
+        cErr = cudaMemcpy(packet_d, packet_p, N_BYTES, cudaMemcpyHostToDevice);
         if (cErr != cudaSuccess) {
             std::string  errMsg = "[CudaRuntime::executeGpuTasks] ";
             errMsg += "Unable to execute H-to-D transfer\n";
@@ -80,9 +111,9 @@ int   main(int argc, char* argv[]) {
             throw std::runtime_error(errMsg);
         }
 
-        gpuKernel::kernel(data_d, nCells);
+        gpuKernel::kernel(packet_d);
 
-        cErr = cudaMemcpy(data_h, data_d, nBytes, cudaMemcpyDeviceToHost);
+        cErr = cudaMemcpy(packet_p, packet_d, N_BYTES, cudaMemcpyDeviceToHost);
         if (cErr != cudaSuccess) {
             std::string  errMsg = "[CudaRuntime::executeGpuTasks] ";
             errMsg += "Unable to execute D-to-H transfer\n";
@@ -90,9 +121,18 @@ int   main(int argc, char* argv[]) {
             errMsg += std::string(cudaGetErrorString(cErr)) + "\n";
             throw std::runtime_error(errMsg);
         }
+
+        std::memcpy((void*)data_h, (void*)data_p, N_CELLS*sizeof(Real));
+
+        data_h = nullptr;
+        data_p = nullptr;
+        data_d = nullptr;
+        ptr_p  = nullptr;
+        ptr_d  = nullptr;
     }
 
-    cErr = cudaFree(data_d);
+    // Release buffers
+    cErr = cudaFree(packet_d);
     if (cErr != cudaSuccess) {
         std::string  errMsg = "[CudaDataPacket::prepareForTransfer] ";
         errMsg += "Unable to free device memory\n";
@@ -100,6 +140,17 @@ int   main(int argc, char* argv[]) {
         errMsg += std::string(cudaGetErrorString(cErr)) + "\n";
         throw std::runtime_error(errMsg);
     }
+    packet_d = nullptr;
+
+    cErr = cudaFreeHost(packet_p);
+    if (cErr != cudaSuccess) {
+        std::string  errMsg = "[CudaDataPacket::prepareForTransfer] ";
+        errMsg += "Unable to free pinned memory\n";
+        errMsg += "Cuda error - " + std::string(cudaGetErrorName(cErr)) + "\n";
+        errMsg += std::string(cudaGetErrorString(cErr)) + "\n";
+        throw std::runtime_error(errMsg);
+    }
+    packet_p = nullptr;
 
     // Check that kernel ran correctly    
     for (auto ti = grid.buildTileIter(LEVEL); ti->isValid(); ti->next()) {
@@ -119,20 +170,23 @@ int   main(int argc, char* argv[]) {
                     if (absErr > 1.0e-12) {
                         std::cout << "Bad DENS at ("
                                   << i << "," << j << "," << k << ") - "
-                                  << absErr << "\n";
+                                  << f(i, j, k, DENS_VAR_C) << " instead of "
+                                  << fExpected << "\n";
                     }
-                    fExpected = 2.1 + 2.0*j;
+                    fExpected = 2.0*j + 2.1;
                     absErr = fabs(f(i, j, k, ENER_VAR_C) - fExpected);
                     if (absErr > 1.0e-12) {
                         std::cout << "Bad ENER at ("
                                   << i << "," << j << "," << k << ") - "
-                                  << absErr << "\n";
+                                  << f(i, j, k, ENER_VAR_C) << " instead of "
+                                  << fExpected << "\n";
                     }
                 }
             }
         }
     }
 
+    // Clean-up
     Grid::instance().destroyDomain();
 }
 
