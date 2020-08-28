c4ced5b452c07189bbd949b9059f6c89e3bb553b
Thomas Klosterman <tklos96@gmail.com>
2020-08-28 19:22:20 -0400

    adding GC Fill test to Grid Unit Test

--- test/Grid/gridUnitTest.cpp
+++ test/Grid/gridUnitTest.cpp
@@ -6,6 +6,7 @@
 #include "Grid_Edge.h"
 #include "Grid_Axis.h"
 #include "setInitialConditions_block.h"
+#include "setInitialInteriorTest.h"
 #include "errorEstBlank.h"
 #include "errorEstMaximal.h"
 #include "gtest/gtest.h"
@@ -287,6 +288,46 @@ TEST_F(GridUnitTest,MultiCellGetters){
         }
     }
 
+}
+
+TEST_F(GridUnitTest,GCFill){
+    Grid& grid = Grid::instance();
+    float eps = 1.0e-14;
+
+    grid.initDomain(Simulation::setInitialInteriorTest,
+                    Simulation::errorEstMaximal);
+
+    grid.fillGuardCells();
+    // Test Guard cell fill
+    Real expected_val = 0.0_wp;
+    for (int lev = 0; lev<=grid.getMaxLevel(); ++lev) {
+    for (auto ti = grid.buildTileIter(lev); ti->isValid(); ti->next()) {
+        std::unique_ptr<Tile> tileDesc = ti->buildCurrentTile();
+
+        RealVect cellCenter = tileDesc->getCenterCoords();
+
+        IntVect lo = tileDesc->lo();
+        IntVect hi = tileDesc->hi();
+        IntVect loGC = tileDesc->loGC();
+        IntVect hiGC = tileDesc->hiGC();
+        FArray4D data = tileDesc->data();
+        for (        int k = loGC.K(); k <= hiGC.K(); ++k) {
+            for (    int j = loGC.J(); j <= hiGC.J(); ++j) {
+                for (int i = loGC.I(); i <= hiGC.I(); ++i) {
+                    IntVect pos{LIST_NDIM(i,j,k)};
+                    if (pos.allGE(lo) && pos.allLE(hi) ) {
+                        continue;
+                    }
+
+                    expected_val  = 1.0_wp;
+
+                    EXPECT_NEAR( expected_val, data(i,j,k,DENS_VAR_C), eps);
+                }
+            }
+        }
+
+    } //iterator loop
+    } //level loop
 }
 
 TEST_F(GridUnitTest,PlotfileOutput){
