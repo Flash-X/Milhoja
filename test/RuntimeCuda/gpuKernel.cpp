6b5e108b405ef5935cc6ac0967b0ee6962e95c31
Jared O'Neal <joneal@anl.gov>
2020-08-29 13:00:41 -0400

    (Issue #8) Working my way up to mapping 4D indexing to 1D indexing so that I can use data_d without FArray4D still.

--- test/RuntimeCuda/gpuKernel.cpp
+++ test/RuntimeCuda/gpuKernel.cpp
@@ -18,22 +18,32 @@ void   gpuKernel::kernel(void* packet_d) {
     p += MDIM * sizeof(int);
     Real*      data_d = reinterpret_cast<Real*>(p);
 
-    std::size_t   n = 0;
+    int   i0 = 0;
+    int   j0 = 0;
+    int   k0 = 0;
+    int   jstride = 0;
+    int   kstride = 0;
+    int   nstride = 0;
 
     // Run our kernel in the device with the anticipation
     // that the data is alredy present in the device memory
-    #pragma acc data create(n) deviceptr(loGC_d, hiGC_d, data_d)
+    #pragma acc data create(i0, j0, k0, jstride, kstride, nstride) \
+                     deviceptr(loGC_d, hiGC_d, data_d)
     {
-    #pragma acc kernels
-    n = (  (hiGC_d[IAXIS_C] - loGC_d[IAXIS_C] + 1)
-         * (hiGC_d[JAXIS_C] - loGC_d[JAXIS_C] + 1)
-         * (hiGC_d[KAXIS_C] - loGC_d[KAXIS_C] + 1)
-         * NUNKVAR);
+        #pragma acc kernels
+        {
+        i0 = loGC_d[IAXIS_C];
+        j0 = loGC_d[JAXIS_C];
+        k0 = loGC_d[KAXIS_C];
+        jstride =            hiGC_d[IAXIS_C] - i0 + 1;
+        kstride = jstride * (hiGC_d[JAXIS_C] - j0 + 1);
+        nstride = kstride * (hiGC_d[KAXIS_C] - k0 + 1);
+        }
 
-    #pragma acc parallel loop default(none)
-    for (int i=0; i<n; ++i) {
-        data_d[i] += 2.1;
-    }
+        #pragma acc parallel loop default(none)
+        for (int i=0; i<NUNKVAR*nstride; ++i) {
+            data_d[i] += 2.1;
+        }
     }
 }
 
