cc472c0bf0a746e7c361d43025fc5b716dfdf8dd
Jared O'Neal <joneal@anl.gov>
2020-08-29 16:07:41 -0400

    (Issue #8) Added loop directive to all loops in kernel's loop nest.  This is done to inform OpenACC that the loops are independent and should be parallelized.  It looks like it parallelized at the level of the k and i loops. The runtime of the kernel decreased by a factor of 7.

--- test/RuntimeCuda/gpuKernel.cpp
+++ test/RuntimeCuda/gpuKernel.cpp
@@ -27,32 +27,16 @@ void   gpuKernel::kernel(void* packet_d) {
     p += N_CELLS*sizeof(Real);
     FArray4D*   f_d = reinterpret_cast<FArray4D*>(p);
 
-    int            i0 = 0;
-    int            j0 = 0;
-    int            k0 = 0;
-    int            in = 0;
-    int            jn = 0;
-    int            kn = 0;
-
     // Run our kernel in the device with the anticipation
     // that the data is alredy present in the device memory
-    #pragma acc data create(i0, j0, k0, in, jn, kn) \
-                     deviceptr(loGC_d, hiGC_d, f_d)
+    #pragma acc data deviceptr(loGC_d, hiGC_d, f_d)
     {
-        #pragma acc kernels
-        {
-        i0 = loGC_d[IAXIS_C];
-        j0 = loGC_d[JAXIS_C];
-        k0 = loGC_d[KAXIS_C];
-        in = hiGC_d[IAXIS_C];
-        jn = hiGC_d[JAXIS_C];
-        kn = hiGC_d[KAXIS_C];
-        }
-
         #pragma acc parallel loop default(none)
-        for         (int k=k0; k<=kn; ++k) {
-            for     (int j=j0; j<=jn; ++j) {
-                for (int i=i0; i<=in; ++i) {
+        for         (int k=loGC_d[KAXIS_C]; k<=hiGC_d[KAXIS_C]; ++k) {
+            #pragma acc loop
+            for     (int j=loGC_d[JAXIS_C]; j<=hiGC_d[JAXIS_C]; ++j) {
+                #pragma acc loop
+                for (int i=loGC_d[IAXIS_C]; i<=hiGC_d[IAXIS_C]; ++i) {
                     (*f_d)(i, j, k, DENS_VAR_C) += 2.1;
                     (*f_d)(i, j, k, ENER_VAR_C) += 2.1;
                 }
