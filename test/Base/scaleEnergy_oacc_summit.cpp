abe0566b884792076de0aeb1f67676b42f271b28
Jared O'Neal <joneal@anl.gov>
2020-09-21 17:50:37 -0400

    (Issue #8) Quick cleaning by moving declaration of variables only used internally to a data section to within the section.

--- test/Base/scaleEnergy_oacc_summit.cpp
+++ test/Base/scaleEnergy_oacc_summit.cpp
@@ -7,26 +7,18 @@ void StaticPhysicsRoutines::scaleEnergy_oacc_summit(const orchestration::IntVect
                                                     orchestration::FArray4D* f_d,
                                                     const orchestration::Real scaleFactor,
                                                     const int streamId_h) {
-    int                    i_s = 0;
-    int                    j_s = 0;
-    int                    k_s = 0;
-    int                    i_e = 0;
-    int                    j_e = 0;
-    int                    k_e = 0;
-    orchestration::Real    x = 0.0;
-    orchestration::Real    y = 0.0;
-
-    #pragma acc data create(i_s, j_s, k_s, i_e, j_e, k_e, x, y) \
-                     deviceptr(lo_d, hi_d, xCoords_d, yCoords_d, f_d)
+    #pragma acc data deviceptr(lo_d, hi_d, xCoords_d, yCoords_d, f_d)
     {
         #pragma acc kernels default(none) async(streamId_h)
         {
-            i_s = lo_d->I();
-            j_s = lo_d->J();
-            k_s = lo_d->K();
-            i_e = hi_d->I();
-            j_e = hi_d->J();
-            k_e = hi_d->K();
+            int                    i_s = lo_d->I();
+            int                    j_s = lo_d->J();
+            int                    k_s = lo_d->K();
+            int                    i_e = hi_d->I();
+            int                    j_e = hi_d->J();
+            int                    k_e = hi_d->K();
+            orchestration::Real    x = 0.0;
+            orchestration::Real    y = 0.0;
             for         (int k=k_s; k<=k_e; ++k) {
                 for     (int j=j_s; j<=j_e; ++j) {
                     y = yCoords_d[j - j_s];
