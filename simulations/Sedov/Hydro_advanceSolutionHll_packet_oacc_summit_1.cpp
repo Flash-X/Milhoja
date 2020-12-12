e0e465d948933cc51a97fca322557ee23c89bd69
Jared O'Neal <joneal@anl.gov>
2020-12-12 16:27:22 -0500

    (Issue #19) Updated the kernel docomposition II action routine so that it runs the kernels concurrently on the GPU where possible.

--- simulations/Sedov/Hydro_advanceSolutionHll_packet_oacc_summit_1.cpp
+++ simulations/Sedov/Hydro_advanceSolutionHll_packet_oacc_summit_1.cpp
@@ -68,7 +68,7 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_1(const int tId,
             Stream         stream2 = sMgr.requestStream(false);
             const int      queue2_h = stream2.accAsyncQueue;
             if (queue2_h == NULL_ACC_ASYNC_QUEUE) {
-                throw std::runtime_error("[Hydro::advanceSolutionHll_packet_oacc_summit_2] "
+                throw std::runtime_error("[Hydro::advanceSolutionHll_packet_oacc_summit_1] "
                                          "Unable to acquire an extra asynchronous queue");
             }
 
@@ -108,7 +108,7 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_1(const int tId,
             Stream         stream2 = sMgr.requestStream(false);
             const int      queue2_h = stream2.accAsyncQueue;
             if (queue2_h == NULL_ACC_ASYNC_QUEUE) {
-                throw std::runtime_error("[Hydro::advanceSolutionHll_packet_oacc_summit_2] "
+                throw std::runtime_error("[Hydro::advanceSolutionHll_packet_oacc_summit_1] "
                                          "Unable to acquire second asynchronous queue");
             }
             Stream         stream3 = sMgr.requestStream(false);
--- simulations/Sedov/Hydro_advanceSolutionHll_packet_oacc_summit_2.cpp
+++ simulations/Sedov/Hydro_advanceSolutionHll_packet_oacc_summit_2.cpp
@@ -2,10 +2,11 @@
 #error "This file should only be compiled if using OpenACC offloading"
 #endif
 
+#include "DataPacket.h"
+#include "StreamManager.h"
+
 #include "Eos.h"
 #include "Hydro.h"
-
-#include "DataPacket.h"
 
 #include "Flash.h"
 
@@ -32,6 +33,35 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
     // happen for all action routines that must filter?
     packet_h->setVariableMask(UNK_VARS_BEGIN_C, EINT_VAR_C);
 
+    //----- ACQUIRE EXTRA STREAMS
+    // FIXME: If we allow this request to block, the code could deadlock.  We
+    // therefore, do not block in favor of aborting execution.  Should the data
+    // packets take a parameter at instantiation that specifies how many streams
+    // it should acquire eagerly?
+    // Acquire extra stream
+    StreamManager& sMgr = StreamManager::instance();
+
+    Stream         stream2 = sMgr.requestStream(false);
+    const int      queue2_h = stream2.accAsyncQueue;
+    if (queue2_h == NULL_ACC_ASYNC_QUEUE) {
+        throw std::runtime_error("[Hydro::advanceSolutionHll_packet_oacc_summit_2] "
+                                 "Unable to acquire second asynchronous queue");
+    }
+
+    Stream         stream3 = sMgr.requestStream(false);
+    const int      queue3_h = stream3.accAsyncQueue;
+    if (queue3_h == NULL_ACC_ASYNC_QUEUE) {
+        throw std::runtime_error("[Hydro::advanceSolutionHll_packet_oacc_summit_2] "
+                                 "Unable to acquire third asynchronous queue");
+    }
+
+    Stream         stream4 = sMgr.requestStream(false);
+    const int      queue4_h = stream4.accAsyncQueue;
+    if (queue4_h == NULL_ACC_ASYNC_QUEUE) {
+        throw std::runtime_error("[Hydro::advanceSolutionHll_packet_oacc_summit_2] "
+                                 "Unable to acquire fourth asynchronous queue");
+    }
+
     //----- ADVANCE SOLUTION
     // Update unk data on interiors only
     //   * It is assumed that the GC are filled already.
@@ -42,7 +72,7 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
         if        (location == PacketDataLocation::CC1) {
             packet_h->setDataLocation(PacketDataLocation::CC2);
 
-            // Compute fluxes
+            //----- COMPUTE FLUXES
             // NOTE: CC2 is used solely by auxC during this stage.  It is then
             // used for computing the updated solutions in the next phase.
             #pragma acc parallel loop gang default(none) async(queue_h)
@@ -54,6 +84,10 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                 hy::computeSoundSpeedHll_oacc_summit(ptrs->lo_d, ptrs->hi_d,
                                                      U_d, auxC_d);
             }
+
+            // The X, Y, and Z fluxes each depend on the speed of sound, but can
+            // be computed independently and therefore concurrently.
+#if   NDIM == 1
             #pragma acc parallel loop gang default(none) async(queue_h)
             for (std::size_t n=0; n<*nTiles_d; ++n) {
                 const PacketContents*  ptrs = contents_d + n;
@@ -65,8 +99,25 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                                                    ptrs->deltas_d,
                                                    U_d, flX_d, auxC_d);
             }
-#if NDIM >= 2
+            // No need for barrier since all kernels are launched on the same
+            // queue for 1D case.
+#elif NDIM == 2
+            // Wait for data to arrive and then launch these two for concurrent
+            // execution
+            #pragma acc wait(queue_h)
+
             #pragma acc parallel loop gang default(none) async(queue_h)
+            for (std::size_t n=0; n<*nTiles_d; ++n) {
+                const PacketContents*  ptrs = contents_d + n;
+                const FArray4D*        U_d    = ptrs->CC1_d;
+                const FArray4D*        auxC_d = ptrs->CC2_d;
+                FArray4D*              flX_d  = ptrs->FCX_d;
+
+                hy::computeFluxesHll_X_oacc_summit(dt_d, ptrs->lo_d, ptrs->hi_d,
+                                                   ptrs->deltas_d,
+                                                   U_d, flX_d, auxC_d);
+            }
+            #pragma acc parallel loop gang default(none) async(queue2_h)
             for (std::size_t n=0; n<*nTiles_d; ++n) {
                 const PacketContents*  ptrs = contents_d + n;
                 const FArray4D*        U_d    = ptrs->CC1_d;
@@ -77,9 +128,36 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                                                    ptrs->deltas_d,
                                                    U_d, flY_d, auxC_d);
             }
-#endif
-#if NDIM == 3
+            // BARRIER - fluxes must all be computed before updating the solution
+            #pragma acc wait(queue_h,queue2_h)
+#elif NDIM == 3
+            // Wait for data to arrive and then launch these three for concurrent
+            // execution
+            #pragma acc wait(queue_h)
+
             #pragma acc parallel loop gang default(none) async(queue_h)
+            for (std::size_t n=0; n<*nTiles_d; ++n) {
+                const PacketContents*  ptrs = contents_d + n;
+                const FArray4D*        U_d    = ptrs->CC1_d;
+                const FArray4D*        auxC_d = ptrs->CC2_d;
+                FArray4D*              flX_d  = ptrs->FCX_d;
+
+                hy::computeFluxesHll_X_oacc_summit(dt_d, ptrs->lo_d, ptrs->hi_d,
+                                                   ptrs->deltas_d,
+                                                   U_d, flX_d, auxC_d);
+            }
+            #pragma acc parallel loop gang default(none) async(queue2_h)
+            for (std::size_t n=0; n<*nTiles_d; ++n) {
+                const PacketContents*  ptrs = contents_d + n;
+                const FArray4D*        U_d    = ptrs->CC1_d;
+                const FArray4D*        auxC_d = ptrs->CC2_d;
+                FArray4D*              flY_d  = ptrs->FCY_d;
+
+                hy::computeFluxesHll_Y_oacc_summit(dt_d, ptrs->lo_d, ptrs->hi_d,
+                                                   ptrs->deltas_d,
+                                                   U_d, flY_d, auxC_d);
+            }
+            #pragma acc parallel loop gang default(none) async(queue3_h)
             for (std::size_t n=0; n<*nTiles_d; ++n) {
                 const PacketContents*  ptrs = contents_d + n;
                 const FArray4D*        U_d    = ptrs->CC1_d;
@@ -90,8 +168,11 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                                                    ptrs->deltas_d,
                                                    U_d, flZ_d, auxC_d);
             }
+            // BARRIER - fluxes must all be computed before updated the solution
+            #pragma acc wait(queue_h,queue2_h,queue3_h)
 #endif
 
+            //----- UPDATE SOLUTIONS IN PLACE
             // Update solutions using separate U data blocks so that different
             // variables can be updated simultaneously
             #pragma acc parallel loop gang default(none) async(queue_h)
@@ -107,6 +188,11 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                                                  U_d, Unew_d,
                                                  flX_d, flY_d, flZ_d);
             }
+
+            // The velocities and energy depend on density, but can be updated
+            // independently and therefore concurrently.
+            #pragma acc wait(queue_h)
+
             #pragma acc parallel loop gang default(none) async(queue_h)
             for (std::size_t n=0; n<*nTiles_d; ++n) {
                 const PacketContents*  ptrs = contents_d + n;
@@ -120,7 +206,7 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                                               U_d, Unew_d,
                                               flX_d, flY_d, flZ_d);
             }
-            #pragma acc parallel loop gang default(none) async(queue_h)
+            #pragma acc parallel loop gang default(none) async(queue2_h)
             for (std::size_t n=0; n<*nTiles_d; ++n) {
                 const PacketContents*  ptrs = contents_d + n;
                 const FArray4D*        U_d    = ptrs->CC1_d;
@@ -133,7 +219,7 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                                               U_d, Unew_d,
                                               flX_d, flY_d, flZ_d);
             }
-            #pragma acc parallel loop gang default(none) async(queue_h)
+            #pragma acc parallel loop gang default(none) async(queue3_h)
             for (std::size_t n=0; n<*nTiles_d; ++n) {
                 const PacketContents*  ptrs = contents_d + n;
                 const FArray4D*        U_d    = ptrs->CC1_d;
@@ -146,7 +232,7 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                                               U_d, Unew_d,
                                               flX_d, flY_d, flZ_d);
             }
-            #pragma acc parallel loop gang default(none) async(queue_h)
+            #pragma acc parallel loop gang default(none) async(queue4_h)
             for (std::size_t n=0; n<*nTiles_d; ++n) {
                 const PacketContents*  ptrs = contents_d + n;
                 const FArray4D*        U_d    = ptrs->CC1_d;
@@ -159,6 +245,15 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
                                                 U_d, Unew_d,
                                                 flX_d, flY_d, flZ_d);
             }
+
+            // BARRIER - Remaining computations depend on updated solutions
+            #pragma acc wait(queue_h,queue2_h,queue3_h,queue4_h)
+
+            // Release streams as early as possible
+            sMgr.releaseStream(stream2);
+            sMgr.releaseStream(stream3);
+            sMgr.releaseStream(stream4);
+
 #ifdef EINT_VAR_C
             #pragma acc parallel loop gang default(none) async(queue_h)
             for (std::size_t n=0; n<*nTiles_d; ++n) {
@@ -187,5 +282,6 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_2(const int tId,
     } // OpenACC data block
 
     #pragma acc wait(queue_h)
+
 }
 
