8bd47eb2c7bbec982987d24c5412c1a7861d66e7
Jared O'Neal <joneal@anl.gov>
2021-03-11 14:47:07 -0500

    (Issue #30) Create a high-resolution timer class for timing each process individually at each timestep.  For the moment, this class is part of the Sedov test problem only.

---
+++ test/Sedov/ProcessTimer.h
@@ -0,0 +1,29 @@
+#ifndef PROCESS_TIMER_H__
+#define PROCESS_TIMER_H__
+
+#include <string>
+#include <fstream>
+
+class ProcessTimer {
+public:
+    ProcessTimer(const std::string& filename,
+                 const std::string& testname,
+                 const unsigned int nDistributorThreads,
+                 const unsigned int nCpuThreads,
+                 const unsigned int nGpuThreads,
+                 const unsigned int nBlocksPerPacket,
+                 const unsigned int nBlocksPerCpuTurn);
+    ~ProcessTimer(void);
+
+    void logTimestep(const unsigned int step, const double wtime_sec);
+
+private:
+    int                 rank_;
+    int                 nProcs_;
+    std::ofstream       fptr_;
+    double*             wtimes_sec_;
+    unsigned int*       blockCounts_;
+};
+
+#endif
+
