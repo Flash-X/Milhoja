46911dd547fc1f0f4d2290ca5aafa23aead9e508
Jared O'Neal <joneal@anl.gov>
2021-04-08 15:00:35 -0400

    (Issue #31) Tailored the Runtime/cuda data packet to the actual problem at hand. No extra data included in packets and no transfers of data that doesn't need to be transferred.
    To regressions in Runtime/cuda test on Summit.

--- test/Runtime/cuda/DataPacket_gpu_1.cpp
+++ test/Runtime/cuda/DataPacket_gpu_1.cpp
@@ -5,14 +5,9 @@
 
 #include "Grid_IntVect.h"
 #include "Grid_RealVect.h"
-#include "Grid_Axis.h"
-#include "Grid_Edge.h"
 #include "Grid.h"
-#include "FArray1D.h"
 #include "FArray4D.h"
 #include "Backend.h"
-
-#include "Driver.h"
 
 #include "Flash.h"
 
@@ -38,14 +33,35 @@ void  DataPacket_gpu_1::pack(void) {
 
     Grid&   grid = Grid::instance();
 
-    // TODO: Deltas should just be placed into the packet once.
+    //----- SCRATCH SECTION
+    // No scratch
+
+    //----- COPY IN SECTION
+    // Data needed in GPU that is not tile-specific
+    location_ = PacketDataLocation::CC1;
+
     std::size_t  nTiles = tiles_.size();
-    std::size_t  nBytesPerPacket =            sizeof(std::size_t) 
-                                   + nTiles * sizeof(PacketContents)
-                                   + nTiles * (         1 * DELTA_SIZE_BYTES
-                                               +        2 * POINT_SIZE_BYTES
-                                               + N_BLOCKS * CC_BLOCK_SIZE_BYTES
-                                               + N_BLOCKS * ARRAY4_SIZE_BYTES);
+    std::size_t  nCopyInBytes =            sizeof(std::size_t)
+                                + nTiles * sizeof(PacketContents);
+    std::size_t  nBlockMetadataPerTileBytes  =   1 * DELTA_SIZE_BYTES
+                                               + 2 * POINT_SIZE_BYTES
+                                               + 2 * ARRAY4_SIZE_BYTES;
+    std::size_t  nCopyInDataPerTileBytes = CC_BLOCK_SIZE_BYTES;
+
+    //----- COPY IN/OUT SECTION
+    // No copy-in/out data
+
+    //----- COPY OUT SECTION
+    std::size_t  nCopyOutDataPerTileBytes = CC_BLOCK_SIZE_BYTES;
+
+    nCopyToGpuBytes_ =            nCopyInBytes
+                       + nTiles * nBlockMetadataPerTileBytes
+                       + nTiles * nCopyInDataPerTileBytes;
+    nReturnToHostBytes_ = nTiles * nCopyOutDataPerTileBytes;
+    std::size_t  nBytesPerPacket =            nCopyInBytes
+                                   + nTiles * nBlockMetadataPerTileBytes
+                                   + nTiles * nCopyInDataPerTileBytes
+                                   + nTiles * nCopyOutDataPerTileBytes;
 
     stream_ = Backend::instance().requestStream(true);
     if (!stream_.isValid()) {
@@ -56,6 +72,14 @@ void  DataPacket_gpu_1::pack(void) {
     Backend::instance().requestGpuMemory(nBytesPerPacket, &packet_p_,
                                          nBytesPerPacket, &packet_d_);
 
+    // Define high-level structure
+    copyInStart_p_       = static_cast<char*>(packet_p_);
+    copyInStart_d_       = static_cast<char*>(packet_d_);
+    copyInOutStart_p_    = copyInStart_p_ + nCopyToGpuBytes_;
+    copyInOutStart_d_    = copyInStart_d_ + nCopyToGpuBytes_;
+    char* copyOutStart_p = copyInOutStart_p_;
+    char* copyOutStart_d = copyInOutStart_d_;
+
     // Store for later unpacking the location in pinned memory of the different
     // blocks.
     if (pinnedPtrs_) {
@@ -63,25 +87,17 @@ void  DataPacket_gpu_1::pack(void) {
     }
     pinnedPtrs_ = new BlockPointersPinned[nTiles];
 
-    // TODO: The test that uses this packet is not yet used for timing purposes.
-    // Therefore, we can get away with a suboptimal packet design.
-    //
-    // This presently ships over more than what is needed and moves all data
-    // both back and forth.  Optimiza when there is time.  Use Sedov packet as
-    // an example.
-    nCopyToGpuBytes_    = nBytesPerPacket;
-    nReturnToHostBytes_ = nBytesPerPacket;
-    copyInStart_p_    = static_cast<char*>(packet_p_);
-    copyInStart_d_    = static_cast<char*>(packet_d_);
-    copyInOutStart_p_ = static_cast<char*>(packet_p_);
-    copyInOutStart_d_ = static_cast<char*>(packet_d_);
+    //----- SCRATCH SECTION
+    // No scratch
 
+    //----- COPY IN SECTION
     // Pointer to the next free byte in the current data packets
     // Should be true by C++ standard
     static_assert(sizeof(char) == 1, "Invalid char size");
-    char*   ptr_p = static_cast<char*>(packet_p_);
-    char*   ptr_d = static_cast<char*>(packet_d_);
+    char*   ptr_p = copyInStart_p_;
+    char*   ptr_d = copyInStart_d_;
 
+    // Non-tile-specific data
     nTiles_d_ = static_cast<std::size_t*>((void*)ptr_d); 
     std::memcpy((void*)ptr_p, (void*)&nTiles, sizeof(std::size_t));
     ptr_p += sizeof(std::size_t);
@@ -92,6 +108,11 @@ void  DataPacket_gpu_1::pack(void) {
     ptr_p += nTiles * sizeof(PacketContents);
     ptr_d += nTiles * sizeof(PacketContents);
 
+    char* CC1_data_p = copyInStart_p_ + nCopyInBytes + nTiles*nBlockMetadataPerTileBytes;
+    char* CC1_data_d = copyInStart_d_ + nCopyInBytes + nTiles*nBlockMetadataPerTileBytes;
+    char* CC2_data_p = copyOutStart_p;
+    char* CC2_data_d = copyOutStart_d;
+
     PacketContents*   tilePtrs_p = contents_p_;
     for (std::size_t n=0; n<nTiles; ++n, ++tilePtrs_p) {
         Tile*   tileDesc_h = tiles_[n].get();
@@ -99,23 +120,21 @@ void  DataPacket_gpu_1::pack(void) {
             throw std::runtime_error("[DataPacket_gpu_1::pack] Bad tileDesc");
         }
  
-        const unsigned int  level  = tileDesc_h->level();
         const RealVect      deltas = tileDesc_h->deltas();
         const IntVect       lo     = tileDesc_h->lo();
         const IntVect       hi     = tileDesc_h->hi();
         const IntVect       loGC   = tileDesc_h->loGC();
         const IntVect       hiGC   = tileDesc_h->hiGC();
         Real*               data_h = tileDesc_h->dataPtr();
-        Real*               CC1_data_d = nullptr;
-        Real*               CC2_data_d = nullptr;
         if (data_h == nullptr) {
             throw std::logic_error("[DataPacket_gpu_1::pack] "
                                    "Invalid pointer to data in host memory");
         }
 
-        // TODO: I think that we should put in padding so that all objects 
-        //       are byte aligned in the device's memory.
-        tilePtrs_p->level = level;
+        pinnedPtrs_[n].CC1_data = static_cast<Real*>((void*)CC1_data_p);
+        pinnedPtrs_[n].CC2_data = static_cast<Real*>((void*)CC2_data_p);
+        std::memcpy((void*)CC1_data_p, (void*)data_h, CC_BLOCK_SIZE_BYTES);
+
         tilePtrs_p->deltas_d = static_cast<RealVect*>((void*)ptr_d);
         std::memcpy((void*)ptr_p, (void*)&deltas, DELTA_SIZE_BYTES);
         ptr_p += DELTA_SIZE_BYTES;
@@ -132,18 +151,6 @@ void  DataPacket_gpu_1::pack(void) {
         ptr_p += POINT_SIZE_BYTES;
         ptr_d += POINT_SIZE_BYTES;
 
-        location_ = PacketDataLocation::CC1;
-        pinnedPtrs_[n].CC1_data = static_cast<Real*>((void*)ptr_p);
-        CC1_data_d  = static_cast<Real*>((void*)ptr_d);
-        std::memcpy((void*)ptr_p, (void*)data_h, CC_BLOCK_SIZE_BYTES);
-        ptr_p += CC_BLOCK_SIZE_BYTES;
-        ptr_d += CC_BLOCK_SIZE_BYTES;
-
-        pinnedPtrs_[n].CC2_data = static_cast<Real*>((void*)ptr_p);
-        CC2_data_d  = static_cast<Real*>((void*)ptr_d);
-        ptr_p += CC_BLOCK_SIZE_BYTES;
-        ptr_d += CC_BLOCK_SIZE_BYTES;
-
         // Create an FArray4D object in host memory but that already points
         // to where its data will be in device memory (i.e. the device object
         // will already be attached to its data in device memory).
@@ -151,16 +158,23 @@ void  DataPacket_gpu_1::pack(void) {
         // IMPORTANT: When this local object is destroyed, we don't want it to
         // affect the use of the copies (e.g. release memory).
         tilePtrs_p->CC1_d = static_cast<FArray4D*>((void*)ptr_d);
-        FArray4D   CC1_d{CC1_data_d, loGC, hiGC, NUNKVAR};
+        FArray4D   CC1_d{static_cast<Real*>((void*)CC1_data_d),
+                         loGC, hiGC, NUNKVAR};
         std::memcpy((void*)ptr_p, (void*)&CC1_d, ARRAY4_SIZE_BYTES);
         ptr_p += ARRAY4_SIZE_BYTES;
         ptr_d += ARRAY4_SIZE_BYTES;
 
         tilePtrs_p->CC2_d = static_cast<FArray4D*>((void*)ptr_d);
-        FArray4D   CC2_d{CC2_data_d, loGC, hiGC, NUNKVAR};
+        FArray4D   CC2_d{static_cast<Real*>((void*)CC2_data_d),
+                         loGC, hiGC, NUNKVAR};
         std::memcpy((void*)ptr_p, (void*)&CC2_d, ARRAY4_SIZE_BYTES);
         ptr_p += ARRAY4_SIZE_BYTES;
         ptr_d += ARRAY4_SIZE_BYTES;
+
+        CC1_data_p += CC_BLOCK_SIZE_BYTES;
+        CC1_data_d += CC_BLOCK_SIZE_BYTES;
+        CC2_data_p += CC_BLOCK_SIZE_BYTES;
+        CC2_data_d += CC_BLOCK_SIZE_BYTES;
     }
 }
 
