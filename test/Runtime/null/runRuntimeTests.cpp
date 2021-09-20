52a21e4b29016a2b1d4e0bc21fb0f6ce008a302a
Jared O Neal <joneal@anl.gov>
2021-09-20 14:27:36 -0500

    (Issue #43) Fix-up the Runtime/null unit test so that MPI Init/Finalize now occur at the level of the test.
    Ran production and debug versions of test with no detection of errors.

--- test/Runtime/null/runRuntimeTests.cpp
+++ test/Runtime/null/runRuntimeTests.cpp
@@ -1,12 +1,22 @@
 #include "OrchestrationLogger.h"
 
+#include <mpi.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char* argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
 
-    orchestration::Logger::instantiate("RuntimeTest.log");
+    MPI_Comm    MILHOJA_MPI_COMM = MPI_COMM_WORLD;
 
-    return RUN_ALL_TESTS();
+    MPI_Init(&argc, &argv);
+
+    orchestration::Logger::instantiate(MILHOJA_MPI_COMM,
+                                       "RuntimeTest.log");
+
+    int    errCode = RUN_ALL_TESTS();
+
+    MPI_Finalize();
+
+    return errCode;
 }
 
