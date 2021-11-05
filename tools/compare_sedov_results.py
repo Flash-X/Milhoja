a7fe2919853c3a7c018870bb46e3080f884732f6
Jared O'Neal <joneal@anl.gov>
2021-11-05 11:04:17 -0500

    Modernize and improve the regression test script.  The appropriate z-slice is now chosen automatically.  Improved filename printing in plot titles.

--- tools/compare_sedov_results.py
+++ tools/compare_sedov_results.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-import os
-import sys
+"""
+Run the script with -h to obtain more information regarding the script.
+"""
 
+import os
+import argparse
+
+import numpy as np
 import datetime as dt
 import subprocess as sbp
 import matplotlib.pyplot as plt
@@ -18,64 +23,82 @@ plt.style.use('ggplot')
 # Conserved quantities bins
 N_BINS = 50
 
-# Index of Z-value at which to get slice of final solution
-# This should be close to the center of the explosion
-#Z_IDX = 128
-Z_IDX = 0
-
 #####----- AESTHETICS - These should be used consistently throughout
 FONTSIZE   = 16
 MARKERSIZE = 5
 
-def print_usage(msg):
-    """
-    Print the given message as well as general program and usage information.
-
-    Parameters:
-        msg - a message to express to the user the error that occurred.
-    """
-    print()
-    print(f'ERROR - {msg}')
-    print()
-    print('Program Information')
-    print('-' * 80)
-    print()
-    print('Command Line Usage')
-    print('-' * 80)
-    print()
+#####----- PROGRAM USAGE INFO
+DESCRIPTION = \
+    "Perform a regression test on results obtained with Milhoja's Sedov\n" \
+    "test problem.  This test should work for both 2D and 3D test setups\n" \
+    "and regardless of whether or not the runtime has been used or which\n" \
+    "thread team configurations were used.\n\n" \
+    "Note that for graphical visualizations, only a single 2D slice at the\n" \
+    "approximate midpoint along the z-direction will be shown.\n"
+FNAME_A_HELP = \
+    'The name of the folder that contains the results of all reference\n' \
+    'Sedov results to use in the comparison\n'
+FNAME_B_HELP = \
+    'The name of the folder that contains the results of all new\n' \
+    'Sedov results to use in the comparison\n'
 
 if __name__ == '__main__':
-    """
-    Run the program with no arguments to obtain general program and usage
-    information.
-    """
-    if len(sys.argv[1:]) != 2:
-        print_usage('Two and only two command line arguments please')
-        exit(1)
-    result_A = Path(sys.argv[1])
-    result_B = Path(sys.argv[2])
+    #####----- SPECIFY COMMAND LINE USAGE
+    parser = argparse.ArgumentParser(description=DESCRIPTION, \
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('A', nargs=1, help=FNAME_A_HELP)
+    parser.add_argument('B', nargs=1, help=FNAME_B_HELP)
 
+    #####----- CONFIRM PROPER EXECUTION ENVIRONMENT
     FCOMPARE_EXE = os.getenv('FCOMPARE_EXE')
     if FCOMPARE_EXE == None:
-        print_usage('Please set FCOMPARE_EXE')
+        print()
+        print('ERROR: Please set FCOMPARE_EXE')
+        print()
+        parser.print_help()
         exit(2)
 
+    #####----- GET COMMAND LINE ARGUMENTS
+    args = parser.parse_args()
+
+    result_A = Path(args.A[0]).resolve()
+    result_B = Path(args.B[0]).resolve()
+
     if   not result_A.is_dir():
-        print_usage(f'{result_A} does not exist or is not a folder')
+        print()
+        print(f'{result_A} does not exist or is not a folder')
+        print()
+        parser.print_help()
         exit(3)
     elif not result_B.is_dir():
-        print_usage(f'{result_B} does not exist or is not a folder')
+        print()
+        print(f'{result_B} does not exist or is not a folder')
+        print()
+        parser.print_help()
         exit(4)
+
+    # Select the name to show in visualizations based on regression
+    # testing needs.  This should be <commit>/<result folder>/<test name>
+    fname_A = os.sep.join(result_A.parts[-3:])
+    fname_B = os.sep.join(result_B.parts[-3:])
 
     fname_data_A   = result_A.joinpath('sedov.dat')
     fname_log_A    = result_A.joinpath('sedov.log')
     fname_plot_A   = result_A.joinpath('sedov_plt_final')
-    fname_timing_A = result_A.joinpath('sedov_timings.dat')
+    fname_timing_A = result_A.joinpath('sedov_timings_hydro.dat')
 
     fname_data_B   = result_B.joinpath('sedov.dat')
     fname_log_B    = result_B.joinpath('sedov.log')
     fname_plot_B   = result_B.joinpath('sedov_plt_final')
-    fname_timing_B = result_B.joinpath('sedov_timings.dat')
+    fname_timing_B = result_B.joinpath('sedov_timings_hydro.dat')
+
+    # The timing filename has changed over time.
+    # TODO: Get rid of this once the name is used consistently between 
+    # current baselines and all tests generating new results to be tested.
+    if not fname_timing_A.exists():
+        fname_timing_A = Path(str(fname_timing_A).replace('_hydro', ''))
+    if not fname_timing_B.exists():
+        fname_timing_B = Path(str(fname_timing_B).replace('_hydro', ''))
 
     print()
     print('Execution started at {} UTC'.format(dt.datetime.utcnow()))
@@ -93,10 +116,6 @@ if __name__ == '__main__':
         iq_A_df = r_A.integral_quantities
         print()
         print(f'Integral Quantities A Check - SUCCESS')
-        print()
-        print('Sedov A Result Summary')
-        print('-' * 80)
-        print(r_A)
         print()
     except ValueError as err:
         print()
@@ -117,10 +136,6 @@ if __name__ == '__main__':
         print()
         print(f'Integral Quantities B Check - SUCCESS')
         print()
-        print('Sedov B Result Summary')
-        print('-' * 80)
-        print(r_B)
-        print()
     except ValueError as err:
         print()
         print(f'Integral Quantities B Check - FAILED')
@@ -139,18 +154,28 @@ if __name__ == '__main__':
         print()
         exit(5)
 
+    print()
+    print('Sedov A Result Summary')
+    print('-' * 80)
+    print(r_A)
+    print()
+    print('Sedov B Result Summary')
+    print('-' * 80)
+    print(r_B)
+    print()
+
     #####----- VISUALIZE CONSERVED QUANTITIES TIMESERIES
     subp = mfig.SubplotParams(left=0.06, right=0.97, top=0.84, bottom=0.10, \
                               hspace=0.35, wspace=0.25)
     fig = plt.figure(num=1, FigureClass=sedov.MplConservedQuantities, \
                             figsize=(15, 7), subplotpars=subp)
-    fig.draw_plot(iq_A_df, N_BINS, f'{result_A}')
+    fig.draw_plot(iq_A_df, N_BINS, fname_A)
 
     subp = mfig.SubplotParams(left=0.06, right=0.97, top=0.84, bottom=0.10, \
                               hspace=0.35, wspace=0.25)
     fig = plt.figure(num=2, FigureClass=sedov.MplConservedQuantities, \
                             figsize=(15, 7), subplotpars=subp)
-    fig.draw_plot(iq_B_df, N_BINS, f'{result_B}')
+    fig.draw_plot(iq_B_df, N_BINS, fname_B)
 
     # Non-conservative integral quantities should track each other closely
     # across runs.  Also the conserved quantities should have similar 
@@ -189,25 +214,26 @@ if __name__ == '__main__':
     z_coords_A = r_A.z_coordinates
     z_coords_B = r_B.z_coordinates
     assert(z_coords_A == z_coords_B)
+    z_idx = int(np.floor(0.5 * len(z_coords_A)))
 
     subp = mfig.SubplotParams(left=0.04, right=0.96, top=0.84, bottom=0.11, \
                               hspace=0.5, wspace=0.97)
     fig = plt.figure(num=3, FigureClass=sedov.MplSolutionComparison, \
                             figsize=(18, 4), subplotpars=subp)
     fig.fontsize = FONTSIZE - 4
-    fig.draw_plot(z_coords_A[Z_IDX], \
-                  r_A, f'{result_A.name}', \
-                  r_B, f'{result_B.name}')
+    fig.draw_plot(z_coords_A[z_idx], \
+                  r_A, fname_A, \
+                  r_B, fname_B)
 
     subp = mfig.SubplotParams(left=0.09, right=0.975, top=0.92, bottom=0.11)
     fig = plt.figure(num=4, FigureClass=sedov.MplWalltimesByStep, \
                             figsize=(12, 6), subplotpars=subp)
-    fig.draw_plot(r_A, N_BINS, f'{result_A.name}', 'ms')
+    fig.draw_plot(r_A, N_BINS, fname_A, 'ms')
 
     subp = mfig.SubplotParams(left=0.09, right=0.975, top=0.92, bottom=0.11)
     fig = plt.figure(num=5, FigureClass=sedov.MplWalltimesByStep, \
                             figsize=(12, 6), subplotpars=subp)
-    fig.draw_plot(r_B, N_BINS, f'{result_B.name}', 'ms')
+    fig.draw_plot(r_B, N_BINS, fname_B, 'ms')
 
     plt.show()
 
