b1009d53b71cd556687aba8941b6b57c99ec4ebb
Jared O Neal <joneal@anl.gov>
2022-04-29 09:37:18 -0500

    (Issue #43) Intel builds need a special flag so that all Fortran routines are compiled to be thread safe.

--- intel.mk
+++ intel.mk
@@ -1,5 +1,8 @@
+# -auto needed so that we compile thread-safe Fortran routines.
+# We should never use -save.
+# https://www.intel.com/content/www/us/en/developer/articles/technical/threading-fortran-applications-for-parallel-performance-on-multi-core-systems.html
 CXXFLAGS_STD = -std=c++14
 CUFLAGS_STD  =
-F90FLAGS_STD = -module $(BUILDDIR) -fexceptions
+F90FLAGS_STD = -module $(BUILDDIR) -fexceptions -auto
 DEPFLAGS = -MT $@ -MMD -MP -MF $(BUILDDIR)/$*.d
 
