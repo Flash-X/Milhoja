8c64a0172cf1b2668f1ca551aadc36ef3a38a914
Jared O'Neal <joneal@anl.gov>
2022-06-03 11:18:00 -0400

    (Issue #43) With nvhpc, prefer -Mrecursive over -mp.

--- nvhpc.mk
+++ nvhpc.mk
@@ -1,12 +1,9 @@
-# If we are building the Fortran interface, then the Fortran compile flags must
-# include the openMP flag (-mp) so that local scope variables in Fortran
-# routines called by the runtime are not allocated in static memory.  This makes
-# the routines thread safe.  I was not able to get thread safety with -Mnosave,
-# -acc=host, or -acc=multicore.
+# -Mrecursive needed so that we compile thread-safe Fortran routines.  This flag
+# ensures that local scope variables are not allocated to static memory.
 # TODO: Figure out how to generate dependence files in the build folder.
 # TODO: Could not find equivalent of -fexceptions for nvfortran
 CXXFLAGS_STD = -std=c++14
 CUFLAGS_STD  = -std=c++14
-F90FLAGS_STD = -module $(BUILDDIR) -mp
+F90FLAGS_STD = -module $(BUILDDIR) -Mrecursive
 DEPFLAGS =
 
