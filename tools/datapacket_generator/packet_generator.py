6b32830a8da3d837006225df5a40e2f77dd744da
Wesley Kwiecinski <wkwiecinski@compute-386-03.cels.anl.gov>
2023-06-01 16:24:11 -0500

    Removed _devptr from nTiles

--- tools/datapacket_generator/packet_generator.py
+++ tools/datapacket_generator/packet_generator.py
@@ -730,7 +730,7 @@ def generate_cpp_header_file(parameters: dict, args):
         # manually generate nTiles getter here
         pinned_and_data_ptrs += f"\tint {N_TILES};\n\tvoid* nTiles{START_P} = nullptr;\n\tvoid* nTiles{START_D} = nullptr;\n"
         private_variables.append(f"\t{SIZE_T} nTiles{BLOCK_SIZE} = 0;\n")
-        getters.append(f"\tint* nTiles{GETTER}(void) const {{ return static_cast<int*>(nTiles{START_D}); }}\n")
+        getters.append(f"\tint* nTiles(void) const {{ return static_cast<int*>(nTiles{START_D}); }}\n")
         getters.append(f"\tint nTiles_host(void) const {{ return {N_TILES}; }}\n")
 
         # Everything in the packet consists of pointers to byte regions
