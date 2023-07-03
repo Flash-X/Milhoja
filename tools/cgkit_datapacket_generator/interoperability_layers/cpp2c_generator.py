5496136c6815e4cadd551d84491e9708790a18d4
Wesley Kwiecinski <wkwiecinski@compute-386-03.cels.anl.gov>
2023-07-03 12:04:56 -0500

    Fixed templates & incorrect arg list for cpp2c layer

--- tools/cgkit_datapacket_generator/interoperability_layers/cpp2c_generator.py
+++ tools/cgkit_datapacket_generator/interoperability_layers/cpp2c_generator.py
@@ -17,7 +17,7 @@ def generate_cpp2c_outer(data: dict):
 
 def insert_host_arguments(data: dict, connectors: dict):
     connectors['host_arguments'] = ['const int queue1_h = packet_h->asynchronousQueue();\n',
-                                    'const int nTiles_h = packet_h->_nTiles_h;\n']
+                                    'const int _nTiles_h = packet_h->_nTiles_h;\n']
     n_streams = data.get(sects.EXTRA_STREAMS, 0)
     connectors['host_arguments'].extend([
         f'const int queue{i}_h = packet_h->extraAsynchronousQueue({i});\n'
@@ -27,6 +27,8 @@ def insert_host_arguments(data: dict, connectors: dict):
     connectors['c2f_argument_list'].extend([
         (f'queue{i}_h', 'const int') for i in range(2, n_streams+2)
     ])
+    connectors['c2f_argument_list'].append( ('_nTiles_h', 'const int'))
+    connectors['c2f_argument_list'].append( ('_nTiles_d', 'const void*') )
     connectors['c2f_argument_list'].extend([
         (f'_{item}_d', 'const void*') for item in data[sects.ORDER]
     ])
@@ -35,8 +37,8 @@ def generate_cpp2c_helper(data: dict):
     connectors = defaultdict(list)
     connectors['c2f_argument_list'] = [ ('packet_h', 'void*') ]
     insert_host_arguments(data, connectors)
-    data.get(sects.GENERAL, {}).pop("nTiles", None)
-    connectors['instance_args'] = [ (key, f'const {dtype}') for key,dtype in data.get(sects.GENERAL, {}).items() ] 
+    # data.get(sects.GENERAL, {}).pop("nTiles", None)
+    connectors['instance_args'] = [ (key, f'const {dtype}') for key,dtype in data.get(sects.GENERAL, {}).items() if key != "nTiles" ] 
 
     connectors['c2f_arguments'] = [ item[0] for item in connectors['c2f_argument_list'] ]
     with open('cg-tpl.cpp2c_helper.cpp', 'w') as helper:
@@ -49,6 +51,7 @@ def generate_cpp2c_helper(data: dict):
             [ ',\n'.join([ f"{item[0]}" for item in connectors['c2f_argument_list']]) ] + 
             
             [ '\n\n/* _connector:device_arguments */\n' ] + 
+            [ 'void* _nTiles_d = static_cast<void*>( packet_h->_nTiles_d );\n'] +
             [ ''.join([ f'void* _{item}_d = static_cast<void*>( packet_h->_{item}_d );\n' for item in data[sects.ORDER] ]) ] +
             
             ['\n/* _connector:instance_args */\n'] +
