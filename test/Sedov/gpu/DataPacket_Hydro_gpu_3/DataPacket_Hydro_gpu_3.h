9b63e2a01f45d0add71b97400486823e3b95cbad
Wesley Kwiecinski <wkwiecinski@compute-386-03.cels.anl.gov>
2023-07-10 15:35:40 -0500

    Added files for testing hydro packet 3

---
+++ test/Sedov/gpu/DataPacket_Hydro_gpu_3/DataPacket_Hydro_gpu_3.h
@@ -0,0 +1,285 @@
+#ifndef CGKIT_DATAPACKET_HYDRO_GPU_3_2ND_PASS_UNIQUE_IFNDEF_H_
+#define CGKIT_DATAPACKET_HYDRO_GPU_3_2ND_PASS_UNIQUE_IFNDEF_H_
+
+#if 0
+_dt_h{dt},
+_dt_d{nullptr},
+_nTiles_h{tiles_.size()},
+_nTiles_d{nullptr},
+_deltas_d{nullptr},
+_lo_d{nullptr},
+_hi_d{nullptr},
+_loAuxC_d{nullptr},
+_loU_d{nullptr},
+_loFl_d{nullptr},
+_U_d{nullptr},
+_auxC_d{nullptr},
+_flX_d{nullptr},
+_flY_d{nullptr},
+_flZ_d{nullptr}
+
+constexpr std::size_t SIZE_DT = pad( sizeof(Real) );
+constexpr std::size_t SIZE_NTILES = pad( sizeof(int) );
+constexpr std::size_t SIZE_TILE_DELTAS = sizeof(RealVect);
+constexpr std::size_t SIZE_TILE_LO = sizeof(IntVect);
+constexpr std::size_t SIZE_TILE_HI = sizeof(IntVect);
+constexpr std::size_t SIZE_LOAUXC = sizeof(IntVect);
+constexpr std::size_t SIZE_LOU = sizeof(IntVect);
+constexpr std::size_t SIZE_LOFL = sizeof(IntVect);
+constexpr std::size_t SIZE_U = (8 + 2 * 1) * (8 + 2 * 1) * (1 + 2 * 0) * (8 - 0 + 1) * sizeof(Real);
+constexpr std::size_t SIZE_AUXC = (8 + 2 * 1) * (8 + 2 * 1) * (1 + 2 * 0) * sizeof(Real);
+constexpr std::size_t SIZE_FLX = ((8 + 2 * 1) + 1) * (8 + 2 * 1) * (1 + 2 * 0) * (5) * sizeof(Real);
+constexpr std::size_t SIZE_FLY = (8 + 2 * 1) * ((8 + 2 * 1) + 1) * (1 + 2 * 0) * (5) * sizeof(Real);
+constexpr std::size_t SIZE_FLZ = (1) * (1) * (1) * (1) * sizeof(Real);
+
+_auxC_d = static_cast<Real*>( static_cast<void*>(ptr_d) );
+ptr_d+= _nTiles_h * SIZE_AUXC;
+
+_flX_d = static_cast<Real*>( static_cast<void*>(ptr_d) );
+ptr_d+= _nTiles_h * SIZE_FLX;
+
+_flY_d = static_cast<Real*>( static_cast<void*>(ptr_d) );
+ptr_d+= _nTiles_h * SIZE_FLY;
+
+_flZ_d = static_cast<Real*>( static_cast<void*>(ptr_d) );
+ptr_d+= _nTiles_h * SIZE_FLZ;
+
+
+Real* dt_p = static_cast<Real*>( static_cast<void*>(ptr_p) );
+_dt_d = static_cast<Real*>( static_cast<void*>(ptr_d) );
+ptr_p+=SIZE_DT;
+ptr_d+=SIZE_DT;
+
+int* nTiles_p = static_cast<int*>( static_cast<void*>(ptr_p) );
+_nTiles_d = static_cast<int*>( static_cast<void*>(ptr_d) );
+ptr_p+=SIZE_NTILES;
+ptr_d+=SIZE_NTILES;
+
+
+RealVect* deltas_p = static_cast<RealVect*>( static_cast<void*>(ptr_p) );
+_deltas_d = static_cast<RealVect*>( static_cast<void*>(ptr_d) );
+ptr_p+=_nTiles_h * SIZE_TILE_DELTAS;
+ptr_d+=_nTiles_h * SIZE_TILE_DELTAS;
+
+IntVect* lo_p = static_cast<IntVect*>( static_cast<void*>(ptr_p) );
+_lo_d = static_cast<IntVect*>( static_cast<void*>(ptr_d) );
+ptr_p+=_nTiles_h * SIZE_TILE_LO;
+ptr_d+=_nTiles_h * SIZE_TILE_LO;
+
+IntVect* hi_p = static_cast<IntVect*>( static_cast<void*>(ptr_p) );
+_hi_d = static_cast<IntVect*>( static_cast<void*>(ptr_d) );
+ptr_p+=_nTiles_h * SIZE_TILE_HI;
+ptr_d+=_nTiles_h * SIZE_TILE_HI;
+
+IntVect* loAuxC_p = static_cast<IntVect*>( static_cast<void*>(ptr_p) );
+_loAuxC_d = static_cast<IntVect*>( static_cast<void*>(ptr_d) );
+ptr_p += _nTiles_h * SIZE_LOAUXC;
+ptr_d += _nTiles_h * SIZE_LOAUXC;
+
+IntVect* loU_p = static_cast<IntVect*>( static_cast<void*>(ptr_p) );
+_loU_d = static_cast<IntVect*>( static_cast<void*>(ptr_d) );
+ptr_p += _nTiles_h * SIZE_LOU;
+ptr_d += _nTiles_h * SIZE_LOU;
+
+IntVect* loFl_p = static_cast<IntVect*>( static_cast<void*>(ptr_p) );
+_loFl_d = static_cast<IntVect*>( static_cast<void*>(ptr_d) );
+ptr_p += _nTiles_h * SIZE_LOFL;
+ptr_d += _nTiles_h * SIZE_LOFL;
+
+char* _f4_U_p = ptr_p;
+_f4_U_d = static_cast<FArray4D*>( static_cast<void*>( ptr_d ) );
+ptr_p += _nTiles_h * sizeof(FArray4D);
+ptr_d += _nTiles_h * sizeof(FArray4D);
+
+char* _f4_auxC_p = ptr_p;
+_f4_auxC_d = static_cast<FArray4D*>( static_cast<void*>( ptr_d ) );
+ptr_p += _nTiles_h * sizeof(FArray4D);
+ptr_d += _nTiles_h * sizeof(FArray4D);
+
+char* _f4_flX_p = ptr_p;
+_f4_flX_d = static_cast<FArray4D*>( static_cast<void*>( ptr_d ) );
+ptr_p += _nTiles_h * sizeof(FArray4D);
+ptr_d += _nTiles_h * sizeof(FArray4D);
+
+char* _f4_flY_p = ptr_p;
+_f4_flY_d = static_cast<FArray4D*>( static_cast<void*>( ptr_d ) );
+ptr_p += _nTiles_h * sizeof(FArray4D);
+ptr_d += _nTiles_h * sizeof(FArray4D);
+
+char* _f4_flZ_p = ptr_p;
+_f4_flZ_d = static_cast<FArray4D*>( static_cast<void*>( ptr_d ) );
+ptr_p += _nTiles_h * sizeof(FArray4D);
+ptr_d += _nTiles_h * sizeof(FArray4D);
+
+
+
+ U_p = static_cast<Real*>( static_cast<void*>(ptr_p) );
+_U_d = static_cast<Real*>( static_cast<void*>(ptr_d) );
+ptr_p+=_nTiles_h * SIZE_U;
+ptr_d+=_nTiles_h * SIZE_U;
+
+
+
+std::memcpy(dt_p, static_cast<void*>(&_dt_h), SIZE_DT);
+std::memcpy(nTiles_p, static_cast<void*>(&_nTiles_h), SIZE_NTILES);
+
+const RealVect deltas = tileDesc_h->deltas();
+const IntVect lo = tileDesc_h->lo();
+const IntVect hi = tileDesc_h->hi();
+const IntVect hiGC = tileDesc_h->hiGC();
+const IntVect loGC = tileDesc_h->loGC();
+const IntVect loAuxC = tileDesc_h->lo() - IntVect{ LIST_NDIM(1, 1, 0) };
+const IntVect loU = tileDesc_h->loGC();
+const IntVect loFl = tileDesc_h->lo();
+
+char_ptr = static_cast<char*>( static_cast<void*>( deltas_p ) ) + n * SIZE_TILE_DELTAS;
+std::memcpy(static_cast<void*>(char_ptr), static_cast<const void*>(&deltas), SIZE_TILE_DELTAS);
+
+char_ptr = static_cast<char*>( static_cast<void*>( lo_p ) ) + n * SIZE_TILE_LO;
+std::memcpy(static_cast<void*>(char_ptr), static_cast<const void*>(&lo), SIZE_TILE_LO);
+
+char_ptr = static_cast<char*>( static_cast<void*>( hi_p ) ) + n * SIZE_TILE_HI;
+std::memcpy(static_cast<void*>(char_ptr), static_cast<const void*>(&hi), SIZE_TILE_HI);
+
+char_ptr = static_cast<char*>( static_cast<void*>( loAuxC_p ) ) + n * SIZE_LOAUXC;
+std::memcpy(static_cast<void*>(char_ptr), static_cast<const void*>(&loAuxC), SIZE_LOAUXC);
+
+char_ptr = static_cast<char*>( static_cast<void*>( loU_p ) ) + n * SIZE_LOU;
+std::memcpy(static_cast<void*>(char_ptr), static_cast<const void*>(&loU), SIZE_LOU);
+
+char_ptr = static_cast<char*>( static_cast<void*>( loFl_p ) ) + n * SIZE_LOFL;
+std::memcpy(static_cast<void*>(char_ptr), static_cast<const void*>(&loFl), SIZE_LOFL);
+
+
+char_ptr = static_cast<char*>( static_cast<void*>(_f4_U_d) ) + n * sizeof(FArray4D);
+FArray4D U_device{ static_cast<Real*>( static_cast<void*>( static_cast<char*>( static_cast<void*>(_U_d) ) + n * SIZE_U)), loGC, hiGC, 8 - 0 + 1};
+char_ptr = _f4_U_p + n * sizeof(FArray4D);
+std::memcpy(static_cast<void*>(char_ptr), static_cast<void*>(&U_device), sizeof(FArray4D));
+
+char_ptr = static_cast<char*>( static_cast<void*>(_f4_auxC_d) ) + n * sizeof(FArray4D);
+FArray4D auxC_device{ static_cast<Real*>( static_cast<void*>( static_cast<char*>( static_cast<void*>(_auxC_d) ) + n * SIZE_AUXC)), loGC, hiGC, 1};
+char_ptr = _f4_auxC_p + n * sizeof(FArray4D);
+std::memcpy(static_cast<void*>(char_ptr), static_cast<void*>(&auxC_device), sizeof(FArray4D));
+
+char_ptr = static_cast<char*>( static_cast<void*>(_f4_flX_d) ) + n * sizeof(FArray4D);
+FArray4D flX_device{ static_cast<Real*>( static_cast<void*>( static_cast<char*>( static_cast<void*>(_flX_d) ) + n * SIZE_FLX)), lo, IntVect{ LIST_NDIM( hi.I()+1, hi.J(), hi.K() ) }, 5};
+char_ptr = _f4_flX_p + n * sizeof(FArray4D);
+std::memcpy(static_cast<void*>(char_ptr), static_cast<void*>(&flX_device), sizeof(FArray4D));
+
+char_ptr = static_cast<char*>( static_cast<void*>(_f4_flY_d) ) + n * sizeof(FArray4D);
+FArray4D flY_device{ static_cast<Real*>( static_cast<void*>( static_cast<char*>( static_cast<void*>(_flY_d) ) + n * SIZE_FLY)), lo, IntVect{ LIST_NDIM( hi.I(), hi.J()+1, hi.K() ) }, 5};
+char_ptr = _f4_flY_p + n * sizeof(FArray4D);
+std::memcpy(static_cast<void*>(char_ptr), static_cast<void*>(&flY_device), sizeof(FArray4D));
+
+char_ptr = static_cast<char*>( static_cast<void*>(_f4_flZ_d) ) + n * sizeof(FArray4D);
+FArray4D flZ_device{ static_cast<Real*>( static_cast<void*>( static_cast<char*>( static_cast<void*>(_flZ_d) ) + n * SIZE_FLZ)), lo, IntVect{ LIST_NDIM( hi.I(), hi.J(), hi.K()+1 ) }, 1};
+char_ptr = _f4_flZ_p + n * sizeof(FArray4D);
+std::memcpy(static_cast<void*>(char_ptr), static_cast<void*>(&flZ_device), sizeof(FArray4D));
+
+
+Real* U_d = tileDesc_h->dataPtr();
+std::size_t offset_U = (8 + 2 * 1) * (8 + 2 * 1) * (1 + 2 * 0) * static_cast<std::size_t>(0);
+std::size_t nBytes_U = (8 + 2 * 1) * (8 + 2 * 1) * (1 + 2 * 0) * ( 8 - 0 + 1 ) * sizeof(Real);
+char_ptr = static_cast<char*>( static_cast<void*>(U_p) ) + n * SIZE_U;
+std::memcpy(static_cast<void*>(char_ptr), static_cast<void*>(U_d + offset_U), nBytes_U);
+
+
+
+Real* U_d = tileDesc_h->dataPtr();
+std::size_t offset_U = (8 + 2 * 1) * (8 + 2 * 1) * (1 + 2 * 0) * static_cast<std::size_t>(0);
+Real*        start_h_U = U_data_h + offset_U;
+const Real*  start_p_U = U_data_p + offset_U;
+std::size_t nBytes_U = (8 + 2 * 1) * (8 + 2 * 1) * (1 + 2 * 0) * ( 7 - 0 + 1 ) * sizeof(RealVect);
+std::memcpy(static_cast<void*>(start_h_U), static_cast<const void*>(start_p_U), nBytes_U);
+
+
+SIZE_DT + SIZE_NTILES + _nTiles_h * sizeof(PacketContents)
+
+(5 * sizeof(FArray4D)) + SIZE_TILE_DELTAS + SIZE_TILE_LO + SIZE_TILE_HI + SIZE_LOAUXC + SIZE_LOU + SIZE_LOFL
+
+0
+
+SIZE_U
+
+0
+
+_dt_h
+
+SIZE_AUXC + SIZE_FLX + SIZE_FLY + SIZE_FLZ
+
+Real* U_data_h = tileDesc_h->dataPtr();
+
+Real* U_data_p = static_cast<Real*>( static_cast<void*>( static_cast<char*>( static_cast<void*>( U_p ) ) + n * SIZE_U ) );
+
+constexpr std::size_t SIZE_U = (8 + 2 * 1) * (8 + 2 * 1) * (1 + 2 * 0) * (8 - 0 + 1) * sizeof(Real);
+
+#endif
+
+#include <Milhoja.h>
+#include <Milhoja_real.h>
+#include <Milhoja_DataPacket.h>
+#include <Milhoja_IntVect.h>
+#include <Milhoja_RealVect.h>
+
+using milhoja::Real;
+using milhoja::FArray4D;
+using milhoja::Stream;
+using milhoja::IntVect;
+using milhoja::RealVect;
+
+class cgkit_dataPacket_Hydro_gpu_3_2nd_pass : public milhoja::DataPacket {
+// class cgkit_dataPacket_Hydro_gpu_3_2nd_pass {
+public:
+    //constructor / destructor
+    cgkit_dataPacket_Hydro_gpu_3_2nd_pass(
+    Real dt
+    
+    );
+    ~cgkit_dataPacket_Hydro_gpu_3_2nd_pass(void);
+
+    //helper methods
+    //this should eventually be milhoja::DataPacket.
+    std::unique_ptr<milhoja::DataPacket> clone(void) const;
+    cgkit_dataPacket_Hydro_gpu_3_2nd_pass(cgkit_dataPacket_Hydro_gpu_3_2nd_pass&) = delete;
+    cgkit_dataPacket_Hydro_gpu_3_2nd_pass(const cgkit_dataPacket_Hydro_gpu_3_2nd_pass&) = delete;
+    cgkit_dataPacket_Hydro_gpu_3_2nd_pass(cgkit_dataPacket_Hydro_gpu_3_2nd_pass&& packet) = delete;
+    cgkit_dataPacket_Hydro_gpu_3_2nd_pass& operator=(cgkit_dataPacket_Hydro_gpu_3_2nd_pass&)       = delete;
+	cgkit_dataPacket_Hydro_gpu_3_2nd_pass& operator=(const cgkit_dataPacket_Hydro_gpu_3_2nd_pass&) = delete;
+	cgkit_dataPacket_Hydro_gpu_3_2nd_pass& operator=(cgkit_dataPacket_Hydro_gpu_3_2nd_pass&& rhs)  = delete;
+
+    void pack(void);
+    void unpack(void);
+
+
+    Real _dt_h;
+    Real* _dt_d;
+    int _nTiles_h;
+    int* _nTiles_d;
+    RealVect* _deltas_d;
+    IntVect* _lo_d;
+    IntVect* _hi_d;
+    IntVect* _loAuxC_d;
+    IntVect* _loU_d;
+    IntVect* _loFl_d;
+    Real* _U_d;
+    Real* U_p;
+    Real* _auxC_d;
+    Real* _flX_d;
+    Real* _flY_d;
+    Real* _flZ_d;
+    FArray4D* _f4_U_d;
+    FArray4D* _f4_auxC_d;
+    FArray4D* _f4_flX_d;
+    FArray4D* _f4_flY_d;
+    FArray4D* _f4_flZ_d;
+    
+
+private:
+    static constexpr std::size_t ALIGN_SIZE=16;
+    static constexpr std::size_t pad(const std::size_t size) {
+        return (((size + ALIGN_SIZE - 1) / ALIGN_SIZE) * ALIGN_SIZE);
+    }
+
+};
+
+#endif
--- test/Sedov/gpu/DataPacket_Hydro_gpu_3/Hydro_advanceSolutionHll_packet_oacc_summit_3.cpp
+++ test/Sedov/gpu/DataPacket_Hydro_gpu_3/Hydro_advanceSolutionHll_packet_oacc_summit_3.cpp
@@ -17,21 +17,27 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_3(const int tId,
     using namespace milhoja;
 
     DataPacket*                packet_h   = dynamic_cast<DataPacket*>(dataItem_h);
+    DataPacket_Hydro_gpu_3*    hydro_pkt = dynamic_cast<DataPacket_Hydro_gpu_3*>(packet_h);
 
     const int                  queue_h    = packet_h->asynchronousQueue();
 #if MILHOJA_NDIM == 3
     const int                  queue2_h   = packet_h->extraAsynchronousQueue(2);
     const int                  queue3_h   = packet_h->extraAsynchronousQueue(3);
 #endif
-    const PacketDataLocation   location   = packet_h->getDataLocation();
-    const PacketContents*      contents_d = packet_h->tilePointers();
+    // const PacketContents*      contents_d = packet_h->tilePointers();
 
-//    const char*  ptr_d = static_cast<char*>(packet_h->copyToGpuStart_gpu());
-//    const std::size_t*  nTiles_d = static_cast<std::size_t*>((void*)ptr_d);
-	const int* nTiles_d = static_cast<int*>( dynamic_cast<DataPacket_Hydro_gpu_3*>(packet_h)->nTiles() );
-//    ptr_d += sizeof(std::size_t);
-	const Real* dt_d = static_cast<Real*>( dynamic_cast<DataPacket_Hydro_gpu_3*>(packet_h)->dt() );
-//    const Real*         dt_d     = static_cast<Real*>((void*)ptr_d);
+	const int* nTiles_d = hydro_pkt->_nTiles_d;
+	const Real* dt_d = hydro_pkt->_dt_d;
+    RealVect* deltas_d = hydro_pkt->_deltas_d;
+    IntVect* lo_d = hydro_pkt->_lo_d;
+    IntVect* hi_d  = hydro_pkt->hi_d;
+    FArray4D* CC1_d = hydro_pkt->_f4_U_d;
+    FArray4D* CC2_d = hydro_pkt->_f4_auxC_d;
+    FArray4D* FCX_d = hydro_pkt->_f4_flX_d;
+    FArray4D* FCY_d = hydro_pkt->_f4_flY_d;
+    FArray4D* FCZ_d = hydro_pkt->_f4_flZ_d;
+    
+
     // This task function neither reads from nor writes to GAME.  While it does
     // read from GAMC, this variable is not written to as part of the task
     // function's work.  Therefore, GAME need not be included in the packet and
@@ -53,12 +59,6 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_3(const int tId,
     // happen for all task functions that must filter?  Selecting the order of
     // variables in memory sounds like part of the larger optimization problem
     // as it affects all data packets.
-//    packet_h->setVariableMask(UNK_VARS_BEGIN, EINT_VAR);
-
-    if (location != PacketDataLocation::CC1) {
-        throw std::runtime_error("[Hydro::advanceSolutionHll_packet_oacc_summit_3] "
-                                 "Input data must be in CC1");
-    }
 
     //----- ADVANCE SOLUTION
     // Update unk data on interiors only
@@ -66,16 +66,15 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_3(const int tId,
     //   * No tiling for now means that computing fluxes and updating the
     //     solution can be fused and the full advance run independently on each
     //     block and in place.
-    #pragma acc data deviceptr(nTiles_d, contents_d, dt_d)
+    #pragma acc data deviceptr(nTiles_d, deltas_d, lo_d, hi_d, CC1_d, CC2_d, FCX_d, FCY_d, FCZ_d, dt_d)
     {
         //----- COMPUTE FLUXES
         #pragma acc parallel loop gang default(none) async(queue_h)
         for (std::size_t n=0; n<*nTiles_d; ++n) {
-            const PacketContents*  ptrs = contents_d + n;
-            const FArray4D*        U_d    = ptrs->CC1_d;
-            FArray4D*              auxC_d = ptrs->CC2_d;
+            const FArray4D*        U_d    = CC1_d + n;
+            FArray4D*              auxC_d = CC2_d + n;
 
-            hy::computeSoundSpeedHll_oacc_summit(ptrs->lo_d, ptrs->hi_d,
+            hy::computeSoundSpeedHll_oacc_summit(lo_d, hi_d,
                                                  U_d, auxC_d);
         }
 
@@ -98,21 +97,20 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_3(const int tId,
 #elif MILHOJA_NDIM == 2
         #pragma acc parallel loop gang default(none) async(queue_h)
         for (std::size_t n=0; n<*nTiles_d; ++n) {
-            const PacketContents*  ptrs = contents_d + n;
-            const FArray4D*        U_d    = ptrs->CC1_d;
-            const FArray4D*        auxC_d = ptrs->CC2_d;
-            FArray4D*              flX_d  = ptrs->FCX_d;
-            FArray4D*              flY_d  = ptrs->FCY_d;
+            const FArray4D*        U_d    = CC1_d + n;
+            const FArray4D*        auxC_d = CC2_d + n;
+            FArray4D*              flX_d  = FCX_d + n;
+            FArray4D*              flY_d  = FCY_d + n;
 
             // It seems like for small 2D blocks, fusing kernels is more
             // efficient than fusing actions (i.e. running the two kernels
             // concurrently).  Too much work for the GPU?  Too much overhead
             // from the stream sync (i.e. OpenACC wait)?
-            hy::computeFluxesHll_X_oacc_summit(dt_d, ptrs->lo_d, ptrs->hi_d,
-                                               ptrs->deltas_d,
+            hy::computeFluxesHll_X_oacc_summit(dt_d, lo_d, hi_d,
+                                               deltas_d,
                                                U_d, flX_d, auxC_d);
-            hy::computeFluxesHll_Y_oacc_summit(dt_d, ptrs->lo_d, ptrs->hi_d,
-                                               ptrs->deltas_d,
+            hy::computeFluxesHll_Y_oacc_summit(dt_d, lo_d, hi_d,
+                                               deltas_d,
                                                U_d, flY_d, auxC_d);
         }
 #elif MILHOJA_NDIM == 3
@@ -162,24 +160,22 @@ void Hydro::advanceSolutionHll_packet_oacc_summit_3(const int tId,
         //----- UPDATE SOLUTIONS IN PLACE
         #pragma acc parallel loop gang default(none) async(queue_h)
         for (std::size_t n=0; n<*nTiles_d; ++n) {
-            const PacketContents*  ptrs = contents_d + n;
-            FArray4D*              U_d   = ptrs->CC1_d;
-            const FArray4D*        flX_d = ptrs->FCX_d;
-            const FArray4D*        flY_d = ptrs->FCY_d;
-            const FArray4D*        flZ_d = ptrs->FCZ_d;
+            FArray4D*              U_d   = CC1_d + n;
+            const FArray4D*        flX_d = FCX_d + n;
+            const FArray4D*        flY_d = FCY_d + n;
+            const FArray4D*        flZ_d = FCZ_d + n;
 
             // NOTE: If MILHOJA_NDIM < 3, then some of the FC[YZ]_d will be garbage.
             //       We therefore assume that this routine will not use
             //       those fluxes associated with axes "above" MILHOJA_NDIM.
-            hy::updateSolutionHll_oacc_summit(ptrs->lo_d, ptrs->hi_d,
+            hy::updateSolutionHll_oacc_summit(lo_d, hi_d,
                                               U_d, flX_d, flY_d, flZ_d);
         }
         #pragma acc parallel loop gang default(none) async(queue_h)
         for (std::size_t n=0; n<*nTiles_d; ++n) {
-            const PacketContents*  ptrs = contents_d + n;
-            FArray4D*              U_d = ptrs->CC1_d;
+            FArray4D*              U_d = CC1_d + n;
 
-            Eos::idealGammaDensIe_oacc_summit(ptrs->lo_d, ptrs->hi_d, U_d);
+            Eos::idealGammaDensIe_oacc_summit(lo_d, hi_d, U_d);
         }
     } // OpenACC data block
 
