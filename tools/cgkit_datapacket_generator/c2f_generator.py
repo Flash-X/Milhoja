801524a10386a611277cce0a1cb16a8912ac2632
Wesley Kwiecinski <wkwiecinski@compute-386-03.cels.anl.gov>
2023-07-20 14:47:27 -0500

    fixed incorrect generated type in f90 layer

--- tools/cgkit_datapacket_generator/c2f_generator.py
+++ tools/cgkit_datapacket_generator/c2f_generator.py
@@ -39,7 +39,7 @@ def generate_hydro_advance_c2f(data):
 
         host_pointers = {
             'packet': {'ftype': None, 'ctype': 'type(C_PTR)'}, 
-            **{ f'queue{i}': {'ftype': 'integer', 'ctype': 'integer(MILHOJA_INT)', 'kind': 'acc_handle_kind'} for i in range(1, data.get('n-extra-streams', 0)+2) }
+            **{ f'queue{i}': {'ftype': 'integer', 'ctype': 'integer(MILHOJA_INT)', 'kind': 'acc_handle_kind'} for i in range(1, data.get(sects.EXTRA_STREAMS, 0)+2) }
         }
 
         arg_order = data[sects.ORDER]
@@ -48,13 +48,14 @@ def generate_hydro_advance_c2f(data):
         gpu_pointers = { 'nTiles': { 'ftype': 'integer', 'ctype': 'type(C_PTR)', 'shape': None} }
 
         for key,itype in data.get(sects.GENERAL, {}).items():
+            print(key, itype)
             ftype = itype.lower()
-            if ftype.endswith('int') == 'int': ftype = 'integer'
+            if 'int' in ftype: ftype = 'integer'
             gpu_pointers[key] = { 'ftype': ftype, 'ctype': 'type(C_PTR)', 'shape': None }
 
         for key,name in data.get(sects.T_MDATA, {}).items():
             ftype = mutil.tile_variable_mapping[name].lower()
-            if ftype.endswith('int'): ftype='integer'
+            if 'int' in ftype: ftype='integer'
             gpu_pointers[name] = { 'ftype': ftype, 'ctype': 'type(C_PTR)', 'shape': [3, 'F_nTiles_h'] }
 
         # TODO: This needs to change once the bounds section in the data packet json is solidified.
@@ -65,24 +66,24 @@ def generate_hydro_advance_c2f(data):
         for section in [sects.T_IN, sects.T_IN_OUT, sects.T_OUT]:
             sect_dict = data.get(section, {})
             for item,data in sect_dict.items():
-                ftype = data['type'].lower()
+                ftype = data[sects.DTYPE].lower()
                 if ftype.endswith('int'): ftype='integer'
-                start = data['start' if 'start' in data else 'start-in']
-                end = data['end' if 'end' in data else 'end-in']
+                start = data[sects.START if sects.START in data else sects.START_IN]
+                end = data[sects.END if sects.END in data else sects.END_IN]
                 gpu_pointers[item] = {
                     'ftype': ftype,
                     'ctype': 'type(C_PTR)',
-                    'shape': data['extents'] + [f'{end} - {start} + 1', 'F_nTiles_h']
+                    'shape': data[sects.EXTENTS] + [f'{end} - {start} + 1', 'F_nTiles_h']
                 }
 
         scratch = data.get(sects.T_SCRATCH, {})
         for item,data in scratch.items():
-            ftype = data['type'].lower()
+            ftype = data[sects.DTYPE].lower()
             if ftype.endswith('int'): ftype='integer'
             gpu_pointers[item] = {
                 'ftype': ftype,
                 'ctype': 'type(C_PTR)',
-                'shape': data['extents'] + ['F_nTiles_h']
+                'shape': data[sects.EXTENTS] + ['F_nTiles_h']
             }
 
         host_pointers.update(extents_set)
