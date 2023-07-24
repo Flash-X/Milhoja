24420e33e13b5d36ed49e41a2d8f27943a4ae95e
Wesley Kwiecinski <wkwiecinski@login5.summit.olcf.ornl.gov>
2023-07-24 15:39:16 -0400

    Variant2 test now uses code generated by the packet generator.

--- test/Sedov/gpu/variant2/Driver_executeSimulation.cpp
+++ test/Sedov/gpu/variant2/Driver_executeSimulation.cpp
@@ -110,7 +110,6 @@ void    Driver::executeSimulation(void) {
     milhoja::Real     dtAfter{RPs.getReal("Driver", "dtAfter")};
     unsigned int      writeEveryNSteps{RPs.getUnsignedInt("Driver", "writeEveryNSteps")};
 
-    const DataPacket_Hydro_gpu_2    packetPrototype;
     while ((nStep <= maxSteps) && (Driver::simTime < tMax)) {
         //----- ADVANCE TIME
         // Don't let simulation time exceed maximum simulation time
@@ -136,7 +135,6 @@ void    Driver::executeSimulation(void) {
             grid.fillGuardCells();
             Timer::stop("GC Fill");
         }
-
         double   tStart = MPI_Wtime();
 //        runtime.executeCpuGpuSplitTasks("Advance Hydro Solution",
 //                                        nDistThreads,
@@ -145,15 +143,23 @@ void    Driver::executeSimulation(void) {
 //                                        hydroAdvance_gpu,
 //                                        packetPrototype,
 //                                        nTilesPerCpuTurn);
-        runtime.executeCpuGpuSplitTasks_timed("Advance Hydro Solution",
-                                              nDistThreads,
-                                              stagger_usec,
-                                              hydroAdvance_cpu,
-                                              hydroAdvance_gpu,
-                                              packetPrototype,
-                                              nTilesPerCpuTurn,
-                                              nStep,
-                                              GLOBAL_COMM);
+//        runtime.executeCpuGpuSplitTasks_timed("Advance Hydro Solution",
+//                                              nDistThreads,
+//                                              stagger_usec,
+//                                              hydroAdvance_cpu,
+//                                              hydroAdvance_gpu,
+//                                              packetPrototype,
+//                                              nTilesPerCpuTurn,
+//                                              nStep,
+//                                              GLOBAL_COMM);
+
+		const DataPacket_Hydro_gpu_2 packetPrototype{Driver::dt};
+		runtime.executeGpuTasks("Advance Hydro Solution",
+                                      nDistThreads,
+                                      stagger_usec,
+                                      hydroAdvance_gpu,
+                                      packetPrototype);
+
         double   wtime_sec = MPI_Wtime() - tStart;
         Timer::start("Gather/Write");
         hydro.logTimestep(nStep, wtime_sec);
