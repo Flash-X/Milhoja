78875c7faa39460940a0ab035355dac3910365a0
Jared O Neal <joneal@anl.gov>
2023-09-20 17:50:00 -0500

    Add milhoja pypkg CI test.  Save coverage report.

--- tools/test_milhoja_installation.py
+++ tools/test_milhoja_installation.py
@@ -13,14 +13,23 @@ def main():
     # ----- SPECIFY COMMAND LINE USAGE
     DESCRIPTION =   "Return status of milhoja python package full testing " \
                   + "as exit code for use with CI\n"
+    VERBOSE_HELP = f"Verbosity level of logging"
     parser = argparse.ArgumentParser(
                 description=DESCRIPTION,
                 formatter_class=argparse.RawTextHelpFormatter
              )
+    parser.add_argument(
+        "--verbose", "-v",
+        type=int, choices=[0, 1, 2], default=1,
+        help=VERBOSE_HELP
+    )
+
+    # ----- GET COMMAND LINE ARGUMENTS
     args = parser.parse_args()
+    verbosity_level = args.verbose
 
     # ----- RUN FULL TEST SUITE
-    if not milhoja.test():
+    if not milhoja.test(verbosity_level):
         sys.exit(1)
 
     sys.exit(0)
