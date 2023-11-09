a02a2833a030f9e7995f02ca2cc58cd143f42eaf
Jared O Neal <joneal@anl.gov>
2023-11-09 07:19:50 -0600

    Update code generation scripts to take n[xyz]b

--- test/Runtime/cpu/generate_code_for_milhoja.py
+++ test/Runtime/cpu/generate_code_for_milhoja.py
@@ -34,6 +34,9 @@ def main():
     DESCRIPTION = "Milhoja's test build system calls this to generate code"
     DESTINATION_HELP = "Pre-existing folder to write files to"
     DIM_HELP = "Dimension of test problem"
+    NXB_HELP = "N cells in each block along x-axis"
+    NYB_HELP = "N cells in each block along y-axis"
+    NZB_HELP = "N cells in each block along z-axis"
     MAKEFILE_HELP = "Filename with path of Makefile to generate"
     OVERWRITE_HELP = "Original files overwritten if given"
     VERBOSE_HELP = "Verbosity level of logging"
@@ -49,6 +52,9 @@ def main():
         "dimension", nargs=1, type=int, choices=[1, 2, 3],
         help=DIM_HELP
     )
+    parser.add_argument("nxb", nargs=1, type=int, help=NXB_HELP)
+    parser.add_argument("nyb", nargs=1, type=int, help=NYB_HELP)
+    parser.add_argument("nzb", nargs=1, type=int, help=NZB_HELP)
     parser.add_argument(
         "--overwrite", action='store_true', required=False,
         help=OVERWRITE_HELP
@@ -65,6 +71,9 @@ def main():
     destination = Path(args.destination[0]).resolve()
     makefile = Path(args.makefile[0]).resolve()
     dimension = args.dimension[0]
+    nxb = args.nxb[0]
+    nyb = args.nyb[0]
+    nzb = args.nzb[0]
     overwrite = args.overwrite
     logger = milhoja.BasicLogger(args.verbose)
 
