7ead4b9d12ec52e9712b97a0ca98e5f0be0fdb3b
Wesley Kwiecinski <wkwiecinski@login4.summit.olcf.ornl.gov>
2023-11-20 12:53:04 -0500

    Added some small todos for removing json library.

--- tools/createSizesJson.cpp
+++ tools/createSizesJson.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-
+#include <tuple>
 #include <nlohmann/json.hpp>
 
 #include <Milhoja.h>
@@ -12,10 +12,16 @@
 #include <Milhoja_FArray3D.h>
 #include <Milhoja_FArray4D.h>
 
-/**
- * TODO: Write explanation.  Include that this assumes that calling code knows
- * what it is doing and that pre-existing files will be overwritten.
- */
+// The primary purpose of this file is to compile the byte size information
+// of all data types used by the data packet, as well as determine the byte
+// alignment value. This is necessary so that memory used by the
+// data packet can be optimized, and any gpu code can avoid illegal access
+// or alignment errors.
+// 
+// Because this script is called when the library is built, it assumes that
+// the calling code knows what it is doing. Therefore, the file that is
+// written by this script will be overwriten. The output of this script,
+// sizes.json, should not be edited manually. 
 int main(int argc, char* argv[]) {
     //----- HARDCODED VALUES
     constexpr int            INDENT = 4;
@@ -32,6 +38,12 @@ int main(int argc, char* argv[]) {
     }
     std::string   filename = argv[1];
 
+// TODO: See todo further down.
+//    using types = std::tuple<
+//        int, unsigned int, real, IntVect,
+//        RealVect, FArray1D, FArray2D,
+//        FArray3D, FArray4D, std::size_t
+//    >;
     //----- CREATE NEW JSON OBJECT
     nlohmann::json sizes = {
         {"byte_align", BYTE_ALIGNMENT},
@@ -52,7 +64,28 @@ int main(int argc, char* argv[]) {
     output.exceptions(std::ofstream::failbit | std::ofstream::badbit);
     try {
         output.open(filename, std::ofstream::out | std::ofstream::trunc);
+        //output << "{" << std::endl;
+
+        // TODO: There should be a better way to print the name of a data
+        //       type and its size by using a std::tuple object as well as
+        //       typeid(T).name(). This way we can create a tuple object filled
+        //       with types, making it easy to loop over as well as add new 
+        //       types if necessary. However, for the sake of getting the 
+        //       datapacket_tests into main, I'll ignore this for now.
+        //output << "    \"byte_align\": " << BYTE_ALIGNMENT << "," << std::endl;
+        //output << "    \"int\": " << sizeof(int) << "," << std::endl;
+        //output << "    \"unsigned int\": " << sizeof(unsigned int) << "," << std::endl;
+        //output << "    \"std::size_t\": " << sizeof(std::size_t) << "," << std::endl;
+        //output << "    \"real\": " << sizeof(milhoja::Real) << "," << std::endl;
+        //output << "    \"IntVect\": " << sizeof(milhoja::IntVect) << "," << std::endl;
+        //output << "    \"RealVect\": " << sizeof(milhoja::RealVect) << "," << std::endl;
+        //output << "    \"FArray1D\": " << sizeof(milhoja::FArray1D) << "," << std::endl;
+        //output << "    \"FArray2D\": " << sizeof(milhoja::FArray2D) << "," << std::endl;
+        //output << "    \"FArray3D\": " << sizeof(milhoja::FArray3D) << "," << std::endl;
+        //output << "    \"FArray4D\": " << sizeof(milhoja::FArray4D) << std::endl;
         output << sizes.dump(INDENT);
+        
+        //output << "}" << std::endl;
         output.close();
     } catch (std::ifstream::failure e) {
         std::cerr << "Unable to open, write to, or close " << filename << std::endl;
