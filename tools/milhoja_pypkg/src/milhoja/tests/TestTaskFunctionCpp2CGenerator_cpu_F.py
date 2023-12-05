83458bdc9707a054b173a8e6c106ce4b0598124b
Wesley Kwiecinski <wkwiecinski@compute-386-03.cels.anl.gov>
2023-12-05 16:38:14 -0600

    Added helper function to task function class for checking if we need to combine certain tile metadata. Fixed code generation for cpp2c layer

--- tools/milhoja_pypkg/src/milhoja/tests/TestTaskFunctionCpp2CGenerator_cpu_F.py
+++ tools/milhoja_pypkg/src/milhoja/tests/TestTaskFunctionCpp2CGenerator_cpu_F.py
@@ -23,22 +23,17 @@ class TestTaskFunctionGenerator_OpenACC_F(milhoja.tests.TestCodeGenerators):
     def testSedovGeneration(self):
         path = _TEST_PATH.joinpath("Sedov")
 
-        hydro_2D = {"json": path.joinpath("gpu_tf_hydro_2D.json"),
+        hydro_2D = {"json": path.joinpath("cpu_tf_hydro_2D_f.json"),
                     "header": None,
                     "header_dim_dependent": False,
-                    "source": path.joinpath("REF_gpu_tf_hydro_2D.F90"),
+                    "source": path.joinpath("REF_cpu_tf_hydro_2D.F90"),
                     "source_dim_dependent": False}
-        hydroFC_2D = {"json": path.joinpath("gpu_tf_hydroFC_2D.json"),
-                      "header": None,
-                      "header_dim_dependent": False,
-                      "source": path.joinpath("REF_gpu_tf_hydroFC_2D.F90"),
-                      "source_dim_dependent": False}
         # Only middle node has concurrent kernel launch
-        hydro_3D = {"json": path.joinpath("gpu_tf_hydro_3D.json"),
+        hydro_3D = {"json": path.joinpath("cpu_tf_hydro_3D_f.json"),
                     "header": None,
                     "header_dim_dependent": False,
-                    "source": path.joinpath("REF_gpu_tf_hydro_3D.F90"),
+                    "source": path.joinpath("REF_cpu_tf_hydro_3D.F90"),
                     "source_dim_dependent": False}
 
         self.run_tests([hydro_3D], [3], _create_generator)
-        self.run_tests([hydro_2D, hydroFC_2D], [2], _create_generator)
+        # self.run_tests([hydro_2D], [2], _create_generator)
