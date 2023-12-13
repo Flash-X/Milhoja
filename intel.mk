4af5ffd0651d3a7675d002ad6c73b7ff50475e39
Jared O Neal <joneal@anl.gov>
2023-12-13 12:34:54 -0600

    Allow for builds with AMReX 23.11.
    The newer versions of AMReX require using a more modern C++ standard.
    
    Libraries built and used successfully with Flash-X in GCE/c04.  No warnings in
    library build logs.

--- intel.mk
+++ intel.mk
@@ -1,7 +1,7 @@
 # -auto needed so that we compile thread-safe Fortran routines.
 # We should never use -save.
 # https://www.intel.com/content/www/us/en/developer/articles/technical/threading-fortran-applications-for-parallel-performance-on-multi-core-systems.html
-CXXFLAGS_STD = -std=c++14
+CXXFLAGS_STD = -std=c++17
 CUFLAGS_STD  =
 F90FLAGS_STD = -module $(BUILDDIR) -fexceptions -auto
 DEPFLAGS = -MT $@ -MMD -MP -MF $(BUILDDIR)/$*.d
