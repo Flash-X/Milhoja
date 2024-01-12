5c2f1d709f4264e322230d5f92e156fafb5db36b
Youngjun Lee <youngjunlee7@gmail.com>
2024-01-11 19:52:03 -0600

    fix VCS syntax

--- tools/milhoja_pypkg/setup.py
+++ tools/milhoja_pypkg/setup.py
@@ -23,7 +23,7 @@ description = "Milhoja AMR-specific parallelization for heterogenous platforms"
 # Changes made to python_requires should be propagated to all tox.ini and all
 # CI build server config files.
 python_requires = ">=3.8"
-code_requires = ["Code-Generation-Toolkit@git+https://github.com/johannrudi/cg-kit#2bc2e18"]
+code_requires = ["Code-Generation-Toolkit@git+https://github.com/johannrudi/cg-kit@2bc2e18a83c5732c351f9967b8d9222ba0fecc3a"]
 test_requires = ["numpy"]
 install_requires = code_requires + test_requires
 
