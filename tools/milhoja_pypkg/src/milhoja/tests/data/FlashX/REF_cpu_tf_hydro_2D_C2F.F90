38d952fe68fefe2ad33f1bef125878e9682d65a6
Wesley Kwiecinski <wkwiecinski@anl.gov>
2024-03-07 14:22:56 -0600

    Fixed most generation tests, removed unused template file. Still need to fix task function assembler test.

--- tools/milhoja_pypkg/src/milhoja/tests/data/FlashX/REF_cpu_tf_hydro_2D_C2F.F90
+++ tools/milhoja_pypkg/src/milhoja/tests/data/FlashX/REF_cpu_tf_hydro_2D_C2F.F90
@@ -1,19 +1,4 @@
-!> @copyright Copyright 2022 UChicago Argonne, LLC and contributors
-!!
-!! @licenseblock
-!! Licensed under the Apache License, Version 2.0 (the "License");
-!! you may not use this file except in compliance with the License.
-!!
-!! Unless required by applicable law or agreed to in writing, software
-!! distributed under the License is distributed on an "AS IS" BASIS,
-!! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-!! See the License for the specific language governing permissions and
-!! limitations under the License.
-!! @endlicenseblock
-!!
-!! @file
-
-#include "Milhoja.h"
+include "Milhoja.h"
 
 subroutine cpu_tf_hydro_C2F(MH_external_hydro_op1_dt, &
                             MH_external_hydro_op1_eosMode, &
@@ -74,33 +59,19 @@ subroutine cpu_tf_hydro_C2F(MH_external_hydro_op1_dt, &
     F_external_hydro_op1_dt = REAL(MH_external_hydro_op1_dt)
     F_external_hydro_op1_eosMode = INT(MH_external_hydro_op1_eosMode)
 
-    CALL C_F_POINTER(C_tile_deltas, F_tile_deltas, shape=[MILHOJA_MDIM])
-    CALL C_F_POINTER(C_tile_hi, F_tile_hi, shape=[MILHOJA_MDIM])
-    CALL C_F_POINTER(C_tile_interior, F_tile_interior, shape=[2, MILHOJA_MDIM])
-    CALL C_F_POINTER(C_tile_lo, F_tile_lo, shape=[MILHOJA_MDIM])
-    CALL C_F_POINTER(C_lbdd_CC_1, F_lbdd_CC_1, shape=[MILHOJA_MDIM+1])
-    ! No need for variable masking in tile wrappers since the full data array is
-    ! in the memory system and available for use.  Always include all variables?
-    ! Makes sense, but how to get that number?
-    CALL C_F_POINTER(C_CC_1, F_CC_1, shape=[8 + 2 * 1 * MILHOJA_K1D, 8 + 2 * 1 * MILHOJA_K2D, 1 + 2 * 1 * MILHOJA_K3D, 9 + 1 - 0])
+    CALL C_F_POINTER(C_tile_deltas, F_tile_deltas, shape=[3])
+    CALL C_F_POINTER(C_tile_hi, F_tile_hi, shape=[3])
+    CALL C_F_POINTER(C_tile_interior, F_tile_interior, shape=[2, 3])
+    CALL C_F_POINTER(C_tile_lo, F_tile_lo, shape=[3])
+    CALL C_F_POINTER(C_lbdd_CC_1, F_lbdd_CC_1, shape=[4])
+    CALL C_F_POINTER(C_CC_1, F_CC_1, shape=[8 + 2 * 1 * MILHOJA_K1D, 8 + 2 * 1 * MILHOJA_K2D, 1 + 2 * 1 * MILHOJA_K3D, 10])
     CALL C_F_POINTER(C_lbdd_scratch_hydro_op1_auxC, F_lbdd_scratch_hydro_op1_auxC, shape=[3])
     CALL C_F_POINTER(C_scratch_hydro_op1_auxC, F_scratch_hydro_op1_auxC, shape=[10, 10, 1])
-    CALL C_F_POINTER(C_lbdd_scratch_hydro_op1_flX, F_lbdd_scratch_hydro_op1_flX, shape=[MILHOJA_MDIM+1])
-    CALL C_F_POINTER(C_lbdd_scratch_hydro_op1_flY, F_lbdd_scratch_hydro_op1_flY, shape=[MILHOJA_MDIM+1])
+    CALL C_F_POINTER(C_lbdd_scratch_hydro_op1_flX, F_lbdd_scratch_hydro_op1_flX, shape=[4])
+    CALL C_F_POINTER(C_lbdd_scratch_hydro_op1_flY, F_lbdd_scratch_hydro_op1_flY, shape=[4])
     CALL C_F_POINTER(C_scratch_hydro_op1_flX, F_scratch_hydro_op1_flX, shape=[9, 8, 1, 5])
     CALL C_F_POINTER(C_scratch_hydro_op1_flY, F_scratch_hydro_op1_flY, shape=[8, 9, 1, 5])
     CALL C_F_POINTER(C_scratch_hydro_op1_flZ, F_scratch_hydro_op1_flZ, shape=[1, 1, 1, 1])
-
-    !write(*,*) ""
-    !write(*,*) "dt      = ", F_external_hydro_op1_dt
-    !write(*,*) "eosMode = ", F_external_hydro_op1_eosMode
-    !write(*,*) "deltas  = ", F_tile_deltas
-    !write(*,*) "lo      = ", F_tile_interior(1, :)
-    !write(*,*) "hi      = ", F_tile_interior(2, :)
-    !write(*,*) "loU     = ", F_lbdd_CC_1
-    !write(*,*) "loAuxC  = ", F_lbdd_scratch_hydro_op1_auxC
-    !write(*,*) "loFl    = ", F_lbdd_scratch_hydro_op1_flX
-    !write(*,*) ""
 
     CALL cpu_tf_hydro_Fortran( &
         F_external_hydro_op1_dt, &
