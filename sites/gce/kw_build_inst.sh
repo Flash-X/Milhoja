8aadf0eef9727964c6e8af4c9b9e4db4e6cdb2c7
Klaus Weide <kweide@uchicago.edu>
2024-07-08 14:01:40 -0500

    cleanup and comment gce build & install script

--- sites/gce/kw_build_inst.sh
+++ sites/gce/kw_build_inst.sh
@@ -1,19 +1,20 @@
 # Buid a Milhoja-ish library and install it in my private space - kw
+#
+# Modify manually in 2 places below to build for 1D or 3D instead of 2D.
 
-#export MILHOJA_CODE_REPO=/home/kweide/projects/OrchestrationRuntime
 export MILHOJA_CODE_REPO=/home/kweide/refact/OrchestrationRuntime
-#export KW_INSTALL_DIR=/home/kweide/.local-milhoja-2d
 export KW_INSTALL_DIR=/nfs/gce/projects/FLASH5/kweide/local-milhoja/2d
 
 set -xve
 cd ${MILHOJA_CODE_REPO} # top of cloned OrchestrationRuntime repo
-export MILHOJA_TEST_REPO=/nfs/gce/projects/Milhoja/MilhojaTest
+
 test -f Makefile.configure   && rm -v  Makefile.configure
 test -d build/               && rm -rv build/
 test -d "${KW_INSTALL_DIR}"  && rm -rv "${KW_INSTALL_DIR}"
 ./configure.py --makefile $PWD/sites/gce/Makefile.site.gnu_mpich --dim 2 --runtime HOSTMEM --offload OpenACC --prefix ${KW_INSTALL_DIR} \
 	       --support_exec --support_push --support_packets
 make clean
+# The following is required on gce for building sizes.json, only needed for --support_packets
 export MILHOJA_TEST_CLONE=/nfs/gce/projects/Milhoja/MilhojaTest
 make -j12 all
 make install
