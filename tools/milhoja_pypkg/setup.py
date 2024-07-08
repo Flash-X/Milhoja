2ac942455391df1fd50b3d42add8918ae0ca334e
Youngjun Lee <youngjunlee7@gmail.com>
2024-07-08 15:48:37 -0500

    add setuptools dependency, update cg-kit

--- tools/milhoja_pypkg/setup.py
+++ tools/milhoja_pypkg/setup.py
@@ -22,10 +22,10 @@ description = "Milhoja AMR-specific parallelization for heterogenous platforms"
 
 # Changes made to python_requires should be propagated to all tox.ini and all
 # CI build server config files.
-# Latest version of cg-kit needs python 3.9
-python_requires = ">=3.9"
-# Assuming cg-kit@2bc2e18a83c5732c351f9967b8d9222ba0fecc3a for now.
-code_requires = ["Code-Generation-Toolkit"]
+# Latest version of cg-kit needs python 3.8
+python_requires = ">=3.8"
+# Assuming cg-kit 0.2.2 for now.
+code_requires = ["setuptools", "Code-Generation-Toolkit"]
 test_requires = ["numpy"]
 install_requires = code_requires + test_requires
 
