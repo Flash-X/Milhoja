30915db7b0e8ac7528cce233a602a06762336369
Klaus Weide <kweide@uchicago.edu>
2025-01-25 00:41:48 -0600

    Remove traces of JSONDIR

--- sites/gce/kw_build_inst.sh
+++ sites/gce/kw_build_inst.sh
@@ -6,7 +6,7 @@ export MILHOJA_CODE_REPO=/home/kweide/projects/Milhoja
 export KW_INSTALL_DIR=/nfs/gce/projects/FLASH5/kweide/local-milhoja/2d
 
 set -xve
-cd ${MILHOJA_CODE_REPO} # top of cloned OrchestrationRuntime repo
+cd ${MILHOJA_CODE_REPO} # top of cloned Milhoja repo
 
 test -f Makefile.configure   && rm -v  Makefile.configure
 test -d build/               && rm -rv build/
@@ -14,6 +14,5 @@ test -d "${KW_INSTALL_DIR}"  && rm -rv "${KW_INSTALL_DIR}"
 ./configure.py --makefile $PWD/sites/gce/Makefile.site.gnu_mpich --dim 2 --runtime HOSTMEM --offload OpenACC --prefix ${KW_INSTALL_DIR} \
 	       --support_exec --support_push --support_packets
 make clean
-# export MILHOJA_TEST_CLONE=/nfs/gce/projects/Milhoja/MilhojaTest
 make -j12 all
 make install
